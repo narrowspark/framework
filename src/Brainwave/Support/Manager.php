7760c413db692ac7526654c1068a998c7f8310e7
anolilab <info@anolilab.de>
2015-05-29 10:10:47 +0200

    added new func to support manager

--- src/Brainwave/Support/Manager.php
+++ src/Brainwave/Support/Manager.php
@@ -12,9 +12,10 @@ namespace Brainwave\Support;
  *
  * @license     http://www.narrowspark.com/license
  *
- * @version     0.9.8-dev
+ * @version     0.10.0-dev
  */
 
+use Brainwave\Contracts\Config\Manager as ConfigManager;
 use Brainwave\Contracts\Support\Manager as ManagerContract;
 use Interop\Container\ContainerInterface as ContainerInteropInterface;
 
@@ -25,7 +26,7 @@ use Interop\Container\ContainerInterface as ContainerInteropInterface;
  *
  * @since   0.10.0-dev
  */
-abstract class Manager implements ManagerContract
+abstract class Manager implements ManagerContract, ConnectorContract
 {
     /**
      * The container instance.
@@ -34,13 +35,204 @@ abstract class Manager implements ManagerContract
      */
     protected $container;
 
-    public function setContainer(ContainerInteropInterface$value='')
+    /**
+     * The config instance.
+     *
+     * @var ConfigManager
+     */
+    protected $config;
+
+    /**
+     * The registered custom driver creators.
+     *
+     * @var array
+     */
+    protected $customCreators = [];
+
+    /**
+     * The array of created "drivers".
+     *
+     * @var array
+     */
+    protected $drivers = [];
+
+    /**
+     * Set a ContainerInteropInterface container
+     *
+     * @param ContainerInteropInterface $container
+     *
+     * @return self
+     */
+    public function setContainer(ContainerInteropInterface $container)
     {
-        # code...
+        $this->container = $container;
+
+        return $this;
     }
 
+    /**
+     * Get container
+     *
+     * @return ContainerInteropInterface
+     */
     public function getContainer()
     {
-        # code...
+        return $this->container;
+    }
+
+    /**
+     * Set a config manager
+     *
+     * @param ConfigManager $config
+     *
+     * @return self
+     */
+    public function setConfig(ConfigManager $config)
+    {
+        $this->config = $config;
+
+        return $this;
+    }
+
+    /**
+     * Get config
+     *
+     * @return ConfigManager
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    /**
+     * Set the default cache driver name.
+     *
+     * @param string $name
+     */
+    abstract public function setDefaultDriver($name);
+
+    /**
+     * Get the default driver name.
+     *
+     * @return string
+     */
+    abstract public function getDefaultDriver();
+
+    /**
+     * Builder.
+     *
+     * @param string $driver  The cache driver to use
+     * @param array  $options
+     *
+     * @throws CacheException
+     *
+     * @return mixed
+     */
+    public function driver($driver, array $options = [])
+    {
+         $driver = $driver ?: $this->getDefaultDriver();
+
+        if (!$this->driverExists($driver)) {
+            throw new \Exception(
+                sprintf('The driver [%s] is not supported by the bundle.', $driver)
+            );
+        }
+
+        // If the given driver has not been created before, we will create the instances
+        // here and cache it so we can return it next time very quickly. If there is
+        // already a driver created by this name, we'll just return that instance.
+        if (!isset($this->drivers[$driver])) {
+            $this->drivers[$driver] = $this->createDriver($driver, $options);
+        }
+
+        return $this->drivers[$driver];
+    }
+
+    /**
+     * Create a new driver instance.
+     *
+     * @param string $driver
+     * @param array  $options
+     *
+     * @return mixed
+     */
+    protected function createDriver($driver, array $options = [])
+    {
+        $method = 'create'.ucfirst($driver).'Driver';
+        $options = array_filter($options);
+
+        // We'll check to see if a creator method exists for the given driver. If not we
+        // will check for a custom driver creator, which allows developers to create
+        // drivers using their own customized driver creator Closure to create it.
+        if (isset($this->customCreators[$driver])) {
+            return $this->callCustomCreator($driver, $options);
+        } elseif (method_exists($this, $method)) {
+            return empty($options) ? $this->$method() : $this->$method($options);
+        }
+
+        throw new \InvalidArgumentException(sprintf('Driver [%s] not supported.', $driver));
+    }
+
+    /**
+     * Call a custom driver creator.
+     *
+     * @param string $driver
+     * @param array  $options
+     *
+     * @return mixed
+     */
+    protected function callCustomCreator($driver, array $options = [])
+    {
+        return $this->customCreators[$driver]($options);
+    }
+
+    /**
+     * Register a custom driver creator Closure.
+     *
+     * @param string   $driver
+     * @param \Closure $callback
+     *
+     * @return $this
+     */
+    public function extend($driver, \Closure $callback)
+    {
+        $this->customCreators[$driver] = $callback;
+
+        return $this;
+    }
+
+    /**
+     * Get all of the created "drivers".
+     *
+     * @return array
+     */
+    public function getDrivers()
+    {
+        return $this->drivers;
+    }
+
+    /**
+     * Check if the given driver is supported.
+     *
+     * @param string $driver
+     *
+     * @return bool
+     */
+    public function driverExists($driver)
+    {
+        return isset($this->supportedDrivers[$driver]);
+    }
+
+    /**
+     * Dynamically call the default driver instance.
+     *
+     * @param string $method
+     * @param array  $parameters
+     *
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        return call_user_func_array([$this->driver(), $method], $parameters);
     }
 }
