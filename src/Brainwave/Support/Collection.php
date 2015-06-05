b5bc886b93a30a72f08e03a91e2a7fad622adbb2
anolilab <info@anolilab.de>
2015-06-05 09:14:14 +0200

    Make callback in Collection::filter optional

--- src/Brainwave/Support/Collection.php
+++ src/Brainwave/Support/Collection.php
@@ -186,13 +186,16 @@ class Collection implements
     /**
      * Run a filter over each of the items.
      *
-     * @param callable $callback
-     *
+     * @param  callable|null  $callback
      * @return static
      */
-    public function filter(callable $callback)
+    public function filter(callable $callback = null)
     {
-        return new static (array_filter($this->data, $callback));
+        if ($callback === null) {
+            return new static(array_filter($this->items));
+        }
+
+        return new static(array_filter($this->items, $callback));
     }
 
     /**
