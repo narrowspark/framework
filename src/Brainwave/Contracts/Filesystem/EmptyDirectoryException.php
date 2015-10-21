4502f61ca8893129d891d1a7a8f27529c91a147c
anolilab <info@anolilab.de>
2015-10-21 12:12:44 +0200

    added new Exceptions

---
+++ src/Brainwave/Contracts/Filesystem/EmptyDirectoryException.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Brainwave\Contracts\Filesystem;
+
+/**
+ * Narrowspark - a PHP 5 framework.
+ *
+ * @author      Daniel Bannert <info@anolilab.de>
+ * @copyright   2015 Daniel Bannert
+ *
+ * @link        http://www.narrowspark.de
+ *
+ * @license     http://www.narrowspark.com/license
+ *
+ * @version     0.10.0-dev
+ */
+
+/**
+ * EmptyDirectoryException.
+ *
+ * @author  Daniel Bannert
+ *
+ * @since   0.10.0-dev
+ */
+class EmptyDirectoryException extends \Exception
+{
+}
