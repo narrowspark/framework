cbd9905a1c9ff6d59b589ac68b78b289e5629bd1
daniel bannert <d.bannert@anolilab.de>
2016-01-07 17:10:04 +0100

    added ssl connection error

--- src/Viserio/Connect/Traits/DetectsLostConnections.php
+++ src/Viserio/Connect/Traits/DetectsLostConnections.php
@@ -25,6 +25,7 @@ trait DetectsLostConnections
             'Error while sending',
             'Operation in progress',
             'decryption failed or bad record mac',
+            'SSL connection has been closed unexpectedly',
         ]);
     }
 }
