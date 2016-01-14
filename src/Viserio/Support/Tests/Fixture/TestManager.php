7cb7bff2381ad3eefed9220dde0ea8fc6d23bb9a
anolilab <info@anolilab.de>
2016-01-14 16:49:41 +0100

    Added tests for Manager

--- src/Viserio/Support/Tests/Fixture/TestManager.php
+++ src/Viserio/Support/Tests/Fixture/TestManager.php
@@ -5,4 +5,41 @@ use Viserio\Support\Manager;
 
 class TestManager extends Manager
 {
+    protected $defaultDriver;
+
+    protected $supportedDrivers = [
+        'test'   => 'test',
+        'config' => 'config',
+        'throw'  => 'throw'
+    ];
+
+    /**
+     * Set the default cache driver name.
+     *
+     * @param string $name
+     */
+    public function setDefaultDriver($name)
+    {
+        $this->defaultDriver = $name;
+    }
+
+    /**
+     * Get the default driver name.
+     *
+     * @return string
+     */
+    public function getDefaultDriver()
+    {
+        return $this->defaultDriver;
+    }
+
+    protected function createTestDriver($config = null)
+    {
+        return true;
+    }
+
+    protected function createConfigDriver($config)
+    {
+        return $config;
+    }
 }
--- src/Viserio/Support/Tests/ManagerTest.php
+++ src/Viserio/Support/Tests/ManagerTest.php
@@ -1,6 +1,80 @@
 <?php
 namespace Viserio\Support\Tests;
 
+use Narrowspark\TestingHelper\Traits\MockeryTrait;
+use Narrowspark\TestingHelper\ArrayContainer;
+use Viserio\Support\Tests\Fixture\TestManager;
+use Viserio\Contracts\Config\Manager as ConfigContract;
+
 class ManagerTest extends \PHPUnit_Framework_TestCase
 {
+    use MockeryTrait;
+
+    public function testDefaultDriverSetGet()
+    {
+        $manager = new TestManager();
+        $manager->setDefaultDriver('testDriver');
+
+        $this->assertSame('testDriver', $manager->getDefaultDriver());
+    }
+
+    public function testConfigSetGet()
+    {
+        $config = $this->mock(ConfigContract::class);
+
+        $manager = new TestManager();
+        $manager->setConfig($config);
+
+        $this->assertSame($config, $manager->getConfig());
+    }
+
+    public function testDriver()
+    {
+        $manager = new TestManager();
+        $setting = ['name' => 'foo'];
+
+        $this->assertTrue($manager->driver('test'));
+        $this->assertEquals($setting, $manager->driver('config', $setting));
+        $this->assertEquals(['test' => true, 'config' => $setting], $manager->getDrivers());
+    }
+
+    public function testCustomeDriver()
+    {
+        $manager = new TestManager();
+        $manager->extend('custom', function () {
+            return 'custom';
+        });
+
+        $this->assertSame('custom', $manager->driver('custom'));
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testDriverToThrowException()
+    {
+        (new TestManager())->driver('dont');
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testCreateDriverToThrowException()
+    {
+        (new TestManager())->driver('throw');
+    }
+
+    public function testCall()
+    {
+        $manager = new TestManager();
+        $manager->extend('call', function () {
+            return new ArrayContainer();
+        });
+        $manager->setDefaultDriver('call');
+
+        $driver = $manager->driver('call');
+
+        $this->assertInstanceOf(ArrayContainer::class, $driver);
+        $this->assertFalse($manager->has('test'));
+    }
 }
