6e7b94d151f67f180423d26c2750dd61ae332e8b
daniel bannert <d.bannert@anolilab.de>
2016-01-18 23:07:27 +0100

    Added some Adapter

---
+++ src/Viserio/Cache/Adapter/PdoCachePool.php
@@ -0,0 +1,28 @@
+<?php
+namespace Viserio\Cache\Adapter;
+
+use Cache\Adapter\Common\AbstractCachePool;
+use Psr\Cache\CacheItemInterface;
+
+class PdoCachePool extends AbstractCachePool
+{
+    protected function fetchObjectFromCache($key)
+    {
+
+    }
+
+    protected function clearAllObjectsFromCache()
+    {
+
+    }
+
+    protected function clearOneObjectFromCache($key)
+    {
+
+    }
+
+    protected function storeItemInCache($key, CacheItemInterface $item, $ttl)
+    {
+
+    }
+}
