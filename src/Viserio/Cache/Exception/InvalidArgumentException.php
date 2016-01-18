fd9962ec8e578ed45974830de73aed5e3a7daaca
anolilab <info@anolilab.de>
2016-01-18 15:56:44 +0100

    Start working on refactoring | removed some old files

--- src/Viserio/Cache/Exception/InvalidArgumentException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Viserio\Cache\Exception;
-
-use Viserio\Contracts\Cache\InvalidArgumentException as ExceptionContract;
-
-class InvalidArgumentException extends \InvalidArgumentException implements ExceptionContract
-{
-}
