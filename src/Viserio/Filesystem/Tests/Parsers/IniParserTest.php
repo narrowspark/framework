1fb6869cf83e11559b3e79d69aaab41acbbdb6b2
anolilab <info@anolilab.de>
2016-01-28 11:43:22 +0100

    Start working on parsers tests

--- src/Viserio/Filesystem/Tests/Parsers/IniParserTest.php
+++ src/Viserio/Filesystem/Tests/Parsers/IniParserTest.php
@@ -3,4 +3,20 @@ namespace Viserio\Filesystem\Tests\Parsers;
 
 class IniParserTest extends \PHPUnit_Framework_TestCase
 {
+    use MockeryTrait;
+
+    public function setUp()
+    {
+        # code...
+    }
+
+    public function testLoad()
+    {
+        # code...
+    }
+
+    public function testDump()
+    {
+        # code...
+    }
 }
