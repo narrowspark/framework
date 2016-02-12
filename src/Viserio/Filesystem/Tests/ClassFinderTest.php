6cf04def5a35ea0ab3857f4661f1aa2355189902
anolilab <info@anolilab.de>
2016-02-12 14:54:34 +0100

    remove class finder

--- src/Viserio/Filesystem/Tests/ClassFinderTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Viserio\Filesystem\Tests;
-
-use Viserio\Filesystem\ClassFinder;
-
-class ClassFinderTest extends \PHPUnit_Framework_TestCase
-{
-}
---
+++ src/Viserio/Filesystem/Tests/FilesystemAdapterTest.php
@@ -0,0 +1,7 @@
+<?php
+namespace Viserio\Filesystem\Tests;
+
+class FilesystemAdapterTest extends \PHPUnit_Framework_TestCase
+{
+
+}
