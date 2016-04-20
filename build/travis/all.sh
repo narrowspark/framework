150c45b747707c137b22b9f6e096f6633376460c
daniel bannert <d.bannert@anolilab.de>
2016-04-20 19:42:00 +0200

    upadte travis

--- build/travis/all.sh
+++ build/travis/all.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 # Disable xdebug when hhvm or when SEND_COVERAGE is false
-if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$CHECK_CS" != true && "$SEND_COVERAGE" != true ]]; then
-  phpenv config-rm xdebug.ini;
-fi
-
-if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$CHECK_CS" != true && "$SEND_COVERAGE" != true && "$HUMBUG" != true ]]; then
+if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && (("$CHECK_CS" != true && "$SEND_COVERAGE" != true && "$HUMBUG" != true) || ("$CHECK_CS" != true && "$SEND_COVERAGE" != true)) ]]; then
   phpenv config-rm xdebug.ini;
 fi
 
