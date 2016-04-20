27d8367e0eb4699533c347948b5c3c60f9582ec6
prisis <d.bannert@anolilab.de>
2016-04-20 17:43:59 +0200

    update script

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -4,7 +4,7 @@
 mkdir -p build/logs
 
 if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$CHECK_CS" = true && "$SEND_COVERAGE" = true ]]; then
-    vendor/bin/phpunit -c phpunit.xml.dist --verbose --coverage-text php://stdout --coverage-html build/logs/report lowUpperBound 35 highLowerBound 70 --coverage-clover build/logs/coverage.xml;
+    vendor/bin/phpunit -c phpunit.xml.dist --verbose --coverage-text="php://stdout" --coverage-clover="build/logs/coverage.xml";
 else
     vendor/bin/phpunit -c phpunit.xml.dist --verbose;
 fi
