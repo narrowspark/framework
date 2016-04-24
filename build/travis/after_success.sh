75082d2ced8588781a344c639d32189650846dfb
daniel bannert <d.bannert@anolilab.de>
2016-04-24 17:24:26 +0200

    update

--- build/travis/after_success.sh
+++ build/travis/after_success.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$CHECK_CS" = true && "$SEND_COVERAGE" = true && "$HUMBUG" != true ]]; then
-    php vendor/bin/codacycoverage clover build/logs/coverage.xml
+    vendor/bin/codacycoverage clover build/logs/coverage.xml
     # Run codecov
     bash <(curl -s https://codecov.io/bash)
 fi
