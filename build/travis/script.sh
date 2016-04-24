b39f96bc1a9c9421e2d094a81b5efbdd86209d6c
daniel bannert <d.bannert@anolilab.de>
2016-04-24 18:54:50 +0200

    add try catch

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -2,19 +2,18 @@
 
 # Create logs dir
 mkdir -p build/logs
+#!/bin/bash
 
-if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$CHECK_CS" = true && "$SEND_COVERAGE" = true ]]; then
-    try {
+set +e
+bash -e <<TRY
+    if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$CHECK_CS" = true && "$SEND_COVERAGE" = true ]]; then
         vendor/bin/phpunit -c phpunit.xml.dist -v --coverage-text="php://stdout" --coverage-xml="build/coverage-xml.xml" --coverage-clover="build/logs/clover.xml";
-    } catch {
-        exit(1)
-    }
-else
-    try {
+    else
         vendor/bin/phpunit -c phpunit.xml.dist -v;
-    } catch {
-        exit(1)
-    }
-fi
+    fi
 
-if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$HUMBUG" = true ]]; then vendor/bin/humbug; fi
+    if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$HUMBUG" = true ]]; then vendor/bin/humbug; fi
+TRY
+if [ $? -ne 0 ]; then
+  echo exit(1)
+fi
