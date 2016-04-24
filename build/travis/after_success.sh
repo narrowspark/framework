9fd25162a6c73235b49839c2fc9d784b3502e7cb
daniel bannert <d.bannert@anolilab.de>
2016-04-24 18:16:03 +0200

    remove script

--- build/travis/after_success.sh
+++ build/travis/after_success.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$CHECK_CS" = true && "$SEND_COVERAGE" = true && "$HUMBUG" != true ]]; then
-    bash vendor/bin/codacycoverage phpunit build/coverage-xml.xml
     # Run codecov
     bash <(curl -s https://codecov.io/bash)
 fi
