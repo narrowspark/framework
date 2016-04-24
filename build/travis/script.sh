2ba7680352168004390087ed1fab498cab9e33f9
daniel bannert <d.bannert@anolilab.de>
2016-04-24 18:10:59 +0200

    fix name

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -4,7 +4,7 @@
 mkdir -p build/logs
 
 if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$CHECK_CS" = true && "$SEND_COVERAGE" = true ]]; then
-    vendor/bin/phpunit -c phpunit.xml.dist -v --coverage-text="php://stdout" --coverage-xml="build/coverage-xml" --coverage-clover="build/logs/clover.xml";
+    vendor/bin/phpunit -c phpunit.xml.dist -v --coverage-text="php://stdout" --coverage-xml="build/coverage-xml.xml" --coverage-clover="build/logs/clover.xml";
 else
     vendor/bin/phpunit -c phpunit.xml.dist -v;
 fi
