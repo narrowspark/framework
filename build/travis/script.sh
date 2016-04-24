dbd9e3a02886722700b7d44563f32501e7628baa
daniel bannert <d.bannert@anolilab.de>
2016-04-24 18:51:35 +0200

    add try catch

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -4,9 +4,17 @@
 mkdir -p build/logs
 
 if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$CHECK_CS" = true && "$SEND_COVERAGE" = true ]]; then
-    vendor/bin/phpunit -c phpunit.xml.dist -v --coverage-text="php://stdout" --coverage-xml="build/coverage-xml.xml" --coverage-clover="build/logs/clover.xml";
+    try {
+        vendor/bin/phpunit -c phpunit.xml.dist -v --coverage-text="php://stdout" --coverage-xml="build/coverage-xml.xml" --coverage-clover="build/logs/clover.xml";
+    } catch {
+        exit(1)
+    }
 else
-    vendor/bin/phpunit -c phpunit.xml.dist -v;
+    try {
+        vendor/bin/phpunit -c phpunit.xml.dist -v;
+    } catch {
+        exit(1)
+    }
 fi
 
 if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$HUMBUG" = true ]]; then vendor/bin/humbug; fi
