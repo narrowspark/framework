ec28f8117689e023d76d20147b2496836a2a50c9
prisis <d.bannert@anolilab.de>
2016-05-17 15:27:06 +0200

    added more tests

--- src/Viserio/Translator/PluralCategorys/Balkan.php
+++ src/Viserio/Translator/PluralCategorys/Balkan.php
@@ -35,25 +35,13 @@ class Balkan implements CategoryContract
     public function category($count)
     {
         $count = $this->normalizeInteger($count);
+        $i = 0;
 
-        if (
-            !is_float($count) &&
-            $count % 10 === 1 &&
-            $count % 100 !== 11
-        ) {
+        if (!is_float($count) && $count % 10 === 1 && $count % 100 !== 11) {
             return 0;
-        } elseif (
-            !is_float($count) &&
-            ($i = $count % 10) >= 2 &&
-            $i <= 4 &&
-            !(($i = $count % 100) >= 12 && $i <= 14)
-        ) {
+        } elseif (!is_float($count) && ($i = $count % 10) >= 2 && $i <= 4 && !(($i = $count % 100) >= 12 && $i <= 14)) {
             return 1;
-        } elseif (
-            !is_float($count) &&
-            ($i = $count % 10) === 0 ||
-            ($i >= 5 && $i <= 9) || (($i = $count % 100) >= 11 && $i <= 14)
-        ) {
+        } elseif (!is_float($count) && ($i = $count % 10) === 0 || ($i >= 5 && $i <= 9) || (($i = $count % 100) >= 11 && $i <= 14)) {
             return 2;
         }
 
