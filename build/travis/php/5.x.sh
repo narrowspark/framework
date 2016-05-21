06a2d53d5bb7106bda871dd6c09eb832ae349c06
daniel bannert <d.bannert@anolilab.de>
2016-05-21 12:57:25 +0200

    fixed #117

--- build/travis/php/5.x.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Install libevent
-if [ "$ADAPTER" = "Libevent" ]; then (echo "yes" | pecl install "channel://pecl.php.net/libevent-0.1.0"); fi
-
-# Install ev
-if [ "$ADAPTER" = "Ev" ]; then (echo "yes" | pecl install ev); fi
