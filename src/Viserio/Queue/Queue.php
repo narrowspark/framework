1fb7bf3d7bdc4a23eb204cf980303c6507739fc9
Daniel Bannert <d.bannert@anolilab.de>
2016-06-01 14:45:46 -0400

    Applied fixes from StyleCI

--- src/Viserio/Queue/Queue.php
+++ src/Viserio/Queue/Queue.php
@@ -8,11 +8,11 @@ use Viserio\Contracts\Queue\Pushable as PushableContract;
 abstract class Queue implements PushableContract
 {
     /**
-     * @param AdapterContract          $adapter     The queue adapter
-     * @param Dispatcher               $dispatcher  The event dispatcher
-     * @param bool                     $failUnknown Whether to fail jobs whose status hasn't been implicitly set:
-     *                                              - true: jobs with status unknown are assumed to have not been consumed (fail)
-     *                                              - false: jobs with status unknown are assumed to have been consumed (delete)
+     * @param AdapterContract $adapter     The queue adapter
+     * @param Dispatcher      $dispatcher  The event dispatcher
+     * @param bool            $failUnknown Whether to fail jobs whose status hasn't been implicitly set:
+     *                                     - true: jobs with status unknown are assumed to have not been consumed (fail)
+     *                                     - false: jobs with status unknown are assumed to have been consumed (delete)
      */
     public function __construct(AdapterContract $adapter, Dispatcher $dispatcher, $failUnknown = false)
     {
