66b06d8cad2309a1cbb5eba25373924c30441ac3
prisis <d.bannert@anolilab.de>
2016-06-22 16:58:38 +0200

    added test for ExceptionInfo

--- src/Viserio/Exception/ExceptionInfo.php
+++ src/Viserio/Exception/ExceptionInfo.php
@@ -1,6 +1,7 @@
 <?php
 namespace Viserio\Exception;
 
+use InvalidArgumentException;
 use Throwable;
 use Narrowspark\HttpStatus\HttpStatus;
 use OutOfBoundsException;
@@ -21,6 +22,8 @@ class ExceptionInfo
         try {
             $info = ['id' => $id, 'code' => $code, 'name' => HttpStatus::getReasonPhrase($code), 'detail' => HttpStatus::getReasonMessage($code)];
         } catch (OutOfBoundsException $error) {
+            $info = ['id' => $id, 'code' => 500, 'name' => HttpStatus::getReasonPhrase(500), 'detail' => HttpStatus::getReasonMessage(500)];
+        } catch (InvalidArgumentException $error) {
             $info = ['id' => $id, 'code' => 500, 'name' => HttpStatus::getReasonPhrase(500), 'detail' => HttpStatus::getReasonMessage(500)];
         }
 
---
+++ src/Viserio/Exception/Tests/ExceptionInfoTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Viserio\Exception\Tests;
+
+use Exception;
+use InvalidArgumentException;
+use Viserio\Exception\ExceptionInfo;
+
+class ExceptionInfoTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBadError()
+    {
+        $info = (new ExceptionInfo())->generate(new Exception('Ooops.'), 'test', 666);
+
+        $expected = ['id' => 'test', 'code' => 500, 'name' => 'Internal Server Error', 'detail' => 'An error has occurred and this resource cannot be displayed.', 'summary' => 'Houston, We Have A Problem.'];
+
+        $this->assertSame($expected, $info);
+    }
+
+    public function testHiddenError()
+    {
+        $info = (new ExceptionInfo())->generate(new InvalidArgumentException('Made another mess.'), 'hi', 503);
+
+        $expected = ['id' => 'hi', 'code' => 503, 'name' => 'Service Unavailable', 'detail' => 'The server is currently unavailable. It may be overloaded or down for maintenance.', 'summary' => 'Houston, We Have A Problem.'];
+
+        $this->assertSame($expected, $info);
+    }
+}
