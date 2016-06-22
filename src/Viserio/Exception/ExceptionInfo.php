fc372d12f43e7ace7340849e9f38139682b6eae2
prisis <d.bannert@anolilab.de>
2016-06-22 16:30:55 +0200

    update to http-status v2

--- src/Viserio/Exception/ExceptionInfo.php
+++ src/Viserio/Exception/ExceptionInfo.php
@@ -19,9 +19,9 @@ class ExceptionInfo
     public function generate(Throwable $exception, string $id, int $code): array
     {
         try {
-            $info = ['id' => $id, 'code' => $code, 'name' => Narrowspark::getReasonPhrase($code), 'detail' => ''];
+            $info = ['id' => $id, 'code' => $code, 'name' => HttpStatus::getReasonPhrase($code), 'detail' => HttpStatus::getReasonMessage($code)];
         } catch (OutOfBoundsException $error) {
-            $info = ['id' => $id, 'code' => 500, 'name' => Narrowspark::getReasonPhrase(500), 'detail' => ''];
+            $info = ['id' => $id, 'code' => 500, 'name' => HttpStatus::getReasonPhrase(500), 'detail' => HttpStatus::getReasonMessage(500)];
         }
 
         $info['summary'] = 'Houston, We Have A Problem.';
