21595e958a7dd2730f48abb95b03830b78c59dae
prisis <d.bannert@anolilab.de>
2016-06-24 13:57:11 +0200

    added csv

--- src/Viserio/Parsers/Formats/Csv.php
+++ src/Viserio/Parsers/Formats/Csv.php
@@ -1,7 +1,8 @@
 <?php
 namespace Viserio\Parsers\Formats;
 
-use Exception;
+use RuntimeException;
+use SplFileObject;
 use Viserio\Contracts\Filesystem\ParseException;
 use Viserio\Contracts\Parsers\Format as FormatContract;
 use Viserio\Filesystem\Filesystem;
@@ -14,6 +15,17 @@ class Csv implements FormatContract
      * @var \Viserio\Filesystem\Filesystem
      */
     protected $files;
+
+    /**
+     * Sets the delimiter, enclosure, and escape character for CSV.
+     *
+     * @var array
+     */
+    private $option = [
+        'delimiter' => ';',
+        'enclosure' => '"',
+        'escape' => '\\',
+    ];
 
     /**
      * Create a new file filesystem loader.
@@ -31,14 +43,27 @@ class Csv implements FormatContract
     public function parse(string $payload): array
     {
         try {
-            if ($this->files->exists($filename)) {
-                return '';
+            if ($this->files->exists($payload)) {
+               $file = new SplFileObject($payload, 'rb');
             }
-        } catch (Exception $exception) {
+        } catch (RuntimeException $exception) {
             throw new ParseException([
                 'message' => 'Failed To Parse Csv',
             ]);
         }
+
+        $array = [];
+
+        $file->setFlags(SplFileObject::READ_CSV | SplFileObject::SKIP_EMPTY);
+        $file->setCsvControl($this->option['delimiter'], $this->option['enclosure'], $this->option['escape']);
+
+        foreach ($file as $data) {
+            if ('#' !== substr($data[0], 0, 1) && isset($data[1]) && 2 === count($data)) {
+                $array[$data[0]] = $data[1];
+            }
+        }
+
+        return $array;
     }
 
     /**
@@ -46,6 +71,21 @@ class Csv implements FormatContract
      */
     public function dump(array $data): string
     {
-        //
+        $handle = fopen('php://memory', 'rb+');
+
+        foreach ($data as $source => $target) {
+            fputcsv(
+                $handle,
+                [$source, $target],
+                $this->option['delimiter'],
+                $this->option['enclosure']
+            );
+        }
+
+        rewind($handle);
+        $output = stream_get_contents($handle);
+        fclose($handle);
+
+        return $output;
     }
 }
