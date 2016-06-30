2295374a429796a694b9a44a179db84f5b0204b7
Daniel Bannert <d.bannert@anolilab.de>
2016-06-30 19:34:18 +0200

    adding empty classes
    Signed-off-by: Daniel Bannert <d.bannert@anolilab.de>

---
+++ src/Viserio/Queue/Connectors/MemoryQueue.php
@@ -0,0 +1,37 @@
+<?php
+namespace Viserio\Queue\Connectors;
+
+class MemoryQueue extends AbstractQueue
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function push(string $job, $data = '', string $queue = null)
+    {
+        //
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function pushRaw(string $payload, string $queue = null, array $options = [])
+    {
+        //
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function later($delay, string $job, $data = '', string $queue = null)
+    {
+        //
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function pop(string $queue = null)
+    {
+        //
+    }
+}
