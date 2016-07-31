89b689e70311e56924493cc35a46363dd077b354
Daniel Bannert <d.bannert@anolilab.de>
2016-08-01 00:24:42 +0200

    fix test

--- src/Viserio/Cache/Tests/CacheManagerTest.php
+++ src/Viserio/Cache/Tests/CacheManagerTest.php
@@ -2,11 +2,17 @@
 declare(strict_types=1);
 namespace Viserio\Cache\Tests;
 
+use Narrowspark\TestingHelper\Traits\MockeryTrait;
 use Viserio\Cache\CacheManager;
 use Viserio\Contracts\Config\Manager as ConfigManager;
+use Cache\Adapter\PHPArray\ArrayCachePool;
 
 class CacheManagerTest extends \PHPUnit_Framework_TestCase
 {
+    use MockeryTrait;
+
+    protected $manager;
+
     public function setUp()
     {
         $this->manager = new CacheManager(
@@ -16,6 +22,10 @@ class CacheManagerTest extends \PHPUnit_Framework_TestCase
 
     public function testArrayPoolCall()
     {
-        $this->assertInstanceOf(, $this->driver('array'));
+        $this->manager->getConfig()->shouldReceive('get')
+            ->once()
+            ->with('cache.drivers', []);
+
+        $this->assertInstanceOf(ArrayCachePool::class, $this->manager->driver('array'));
     }
 }
