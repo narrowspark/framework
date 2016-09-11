ee8e0b0b72244dbc9a948089e81f36c9b4146672
Daniel Bannert <d.bannert@anolilab.de>
2016-09-11 17:59:12 +0000

    Applied fixes from StyleCI

--- src/Viserio/Database/Tratis/ReadonlyTrait.php
+++ src/Viserio/Database/Tratis/ReadonlyTrait.php
@@ -5,18 +5,18 @@ namespace Viserio\Database\Traits;
 trait Readonly
 {
     /**
-     * @inheritDoc
+     * {@inheritdoc}
      */
     public function delete(ConnectionInterface $con = null)
     {
-        throw new PropelException("This is a readonly object.");
+        throw new PropelException('This is a readonly object.');
     }
 
     /**
-     * @inheritDoc
+     * {@inheritdoc}
      */
     public function save(ConnectionInterface $con = null)
     {
-        throw new PropelException("This is a readonly object.");
+        throw new PropelException('This is a readonly object.');
     }
 }
