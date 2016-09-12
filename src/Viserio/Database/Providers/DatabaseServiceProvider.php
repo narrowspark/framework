775eee357b0c73d5247fede35598be19bbf7a369
Daniel Bannert <d.bannert@anolilab.de>
2016-09-12 17:13:59 +0200

    start working on new orm

--- src/Viserio/Database/Providers/DatabaseServiceProvider.php
+++ src/Viserio/Database/Providers/DatabaseServiceProvider.php
@@ -2,10 +2,9 @@
 declare(strict_types=1);
 namespace Viserio\Database\Providers;
 
+use Interop\Container\ContainerInterface;
+use Viserio\Contracts\Config\Manager as ManagerContract;
 use Interop\Container\ServiceProvider;
-use Viserio\Database\Connection\ConnectionFactory;
-use Viserio\Database\DatabaseManager;
-use Viserio\Database\Query;
 
 class DatabaseServiceProvider implements ServiceProvider
 {
@@ -14,63 +13,12 @@ class DatabaseServiceProvider implements ServiceProvider
      */
     public function getServices()
     {
-        if ($this->app->get('config')->get('database::frozen')) {
-            $this->app->bind('db', function () {
-                return 'Database is frozen.';
-            });
-        } else {
-            $this->registerConnectionFactory();
-
-            // The database manager is used to resolve various connections, since multiple
-            // connections might be managed. It also implements the connection resolver
-            // interface which may be used by other components requiring connections.
-            $this->app->singleton('db', function ($app) {
-                return new DatabaseManager(
-                    $app,
-                    $app->get('db.factory')
-                );
-            });
-
-            $this->registerDatabaseQuery();
-        }
+        return [];
     }
 
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return string[]
-     */
-    public function provides(): array
+    public static function createRuntimeConfiguration(ContainerInterface $container)
     {
-        return [
-            'db',
-            'db.factory',
-            'db.query',
-        ];
-    }
+        $config = $container->get(ManagerContract::class)->get('database');
 
-    protected function registerDatabaseQuery()
-    {
-        $type = $this->app->get('db')->getConnections();
-
-        $$this->app->singleton('db.query', function ($app) {
-            return new Query($app->get('db')->connection());
-        });
-
-        foreach ($type as $driver => $value) {
-            $this->app->bind(sprintf('db.%d.query', $driver), function ($app) use ($driver) {
-                return new Query($app->get('db')->connection($driver));
-            });
-        }
-    }
-
-    protected function registerConnectionFactory()
-    {
-        // The connection factory is used to create the actual connection instances on
-        // the database. We will inject the factory into the manager so that it may
-        // make the connections while they are actually needed and not of before.
-        $this->app->singleton('db.factory', function ($app) {
-            return new ConnectionFactory($app);
-        });
     }
 }
