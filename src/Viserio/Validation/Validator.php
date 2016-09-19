1e70c9d054f7f1692ffc5e6e862d319a92645f46
Daniel Bannert <d.bannert@anolilab.de>
2016-09-19 21:24:51 +0200

    update composer

--- src/Viserio/Validation/Validator.php
+++ src/Viserio/Validation/Validator.php
@@ -2,6 +2,9 @@
 declare(strict_types=1);
 namespace Viserio\Validation;
 
-class Validator
+use Respect\Validation\Validator as RespectValidator;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class Validator extends RespectValidator
 {
 }
