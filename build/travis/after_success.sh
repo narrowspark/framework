dbfcd43fdc554597f2e458cadb7b849bc8479420
Daniel Bannert <d.bannert@anolilab.de>
2016-09-19 20:05:13 +0200

    change ext

--- build/travis/after_success.sh
+++ build/travis/after_success.sh
@@ -4,5 +4,5 @@ if [[ "$CHECK_CS" = true && "$SEND_COVERAGE" = true ]]; then
     # Run codecov
     bash <(curl -s https://codecov.io/bash)
     wget https://scrutinizer-ci.com/ocular.phar
-    php ocular.phar code-coverage:upload --access-token="a8a3ec5b2ec383bfeb6c161acd5950fffb54e507a27cf61646e28318285c31c3" --format=php-clover coverage.clover
+    php ocular.phar code-coverage:upload --access-token="a8a3ec5b2ec383bfeb6c161acd5950fffb54e507a27cf61646e28318285c31c3" --format=php-clover coverage.xml
 fi
