ef175bcc4ce3bf18a06fcdd173e192568211de30
Daniel Bannert <d.bannert@anolilab.de>
2016-09-19 19:29:11 +0000

    Applied fixes from StyleCI

--- src/Viserio/Validation/Validator.php
+++ src/Viserio/Validation/Validator.php
@@ -2,8 +2,8 @@
 declare(strict_types=1);
 namespace Viserio\Validation;
 
-use Respect\Validation\Validator as RespectValidator;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Respect\Validation\Validator as RespectValidator;
 
 class Validator extends RespectValidator
 {
