d480514914cf27a51c330cc64a07f60deed7b98b
Daniel Bannert <d.bannert@anolilab.de>
2016-09-24 20:00:02 +0200

    remove relations folder

---
+++ src/Viserio/Database/Query/Builder.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Database\Query;
+
+class Builder
+{
+    /**
+     * The database connection instance.
+     *
+     * @var \Viserio\Database\Connection
+     */
+    protected $connection;
+}
