a5befac847215a8736eef59e783e1b7aeb1fceb6
Daniel Bannert <d.bannert@anolilab.de>
2016-10-11 21:20:18 +0200

    start

---
+++ src/Viserio/Cron/Cron.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Cron;
+
+class Cron
+{
+    public function __construct()
+    {
+        # code...
+    }
+}
---
+++ src/Viserio/Cron/Schedule.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Cron;
+
+class Schedule
+{
+    public function __construct()
+    {
+        # code...
+    }
+}
