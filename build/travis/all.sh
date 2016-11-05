6d2cbc7ebbbe771f260e7a5efe60aadeee8b98f7
Daniel Bannert <d.bannert@anolilab.de>
2016-11-05 10:31:15 +0100

    added chmod

--- build/travis/all.sh
+++ build/travis/all.sh
@@ -5,6 +5,8 @@ if [[ "$DISABLE_XDEBUG" = true ]]; then
   phpenv config-rm xdebug.ini;
 fi
 
+chmod a+x ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
 echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # Install mongodb
