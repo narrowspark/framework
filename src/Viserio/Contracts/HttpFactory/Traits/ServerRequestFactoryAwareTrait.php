d087ece57f702f87a4379b5930f867d05c1703fc
Daniel Bannert <daniel.bannert@trnd.com>
2016-12-15 12:58:26 +0100

    added new traits

---
+++ src/Viserio/Contracts/HttpFactory/Traits/ServerRequestFactoryAwareTrait.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Contracts\HttpFactory\Traits;
+
+use Interop\Http\Factory\ServerRequestInterface;
+use RuntimeException;
+
+trait ServerRequestFactoryAwareTrait
+{
+    /**
+     * A ServerRequest instance.
+     *
+     * @var \Interop\Http\Factory\ServerRequestInterface
+     */
+    protected $serverRequest;
+
+    /**
+     * Set a ServerRequest instance.
+     *
+     * @param \Interop\Http\Factory\ServerRequestInterface $serverRequest
+     *
+     * @return $this
+     */
+    public function setServerRequest(ServerRequestInterface $serverRequest)
+    {
+        $this->serverRequest = $serverRequest;
+
+        return $this;
+    }
+
+    /**
+     * Get the ServerRequest instance.
+     *
+     * @throws \RuntimeException
+     *
+     * @return \Interop\Http\Factory\ServerRequestInterface
+     */
+    public function getServerRequest(): ServerRequestInterface
+    {
+        if (! $this->serverRequest) {
+            throw new RuntimeException('ServerRequest is not set up.');
+        }
+
+        return $this->serverRequest;
+    }
+}
