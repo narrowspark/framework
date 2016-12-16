27238d9926fc85d328bfee95a470e66392249d5a
Daniel Bannert <daniel.bannert@trnd.com>
2016-12-16 16:00:02 +0100

    adding profile class

--- src/Viserio/WebProfiler/Profile.php
+++ src/Viserio/WebProfiler/Profile.php
@@ -2,6 +2,9 @@
 declare(strict_types=1);
 namespace Viserio\WebProfiler;
 
+use InvalidArgumentException;
+use Viserio\Contracts\WebProfiler\DataCollector as DataCollectorContract;
+
 class Profile
 {
     /**
@@ -45,4 +48,227 @@ class Profile
      * @var string
      */
     private $statusCode;
+
+    /**
+     * [$collectors description]
+     *
+     * @var array
+     */
+    private $collectors = [];
+
+    /**
+     * Sets the token.
+     *
+     * @param string $token The token
+     *
+     * @return void
+     */
+    public function setToken(string $token): void
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Gets the token.
+     *
+     * @return string The token
+     */
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    /**
+     * Returns the IP.
+     *
+     * @return string The IP
+     */
+    public function getIp(): string
+    {
+        return $this->ip;
+    }
+
+    /**
+     * Sets the IP.
+     *
+     * @param string $ip
+     *
+     * @return void
+     */
+    public function setIp(string $ip): void
+    {
+        $this->ip = $ip;
+    }
+
+    /**
+     * Returns the request method.
+     *
+     * @return string The request method
+     */
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    /**
+     * [setMethod description]
+     *
+     * @param string $method
+     *
+     * @return void
+     */
+    public function setMethod(string $method): void
+    {
+        $this->method = $method;
+    }
+
+    /**
+     * Returns the URL.
+     *
+     * @return string The URL
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * [setUrl description]
+     *
+     * @param string $url
+     *
+     * @return void
+     */
+    public function setUrl(string $url): void
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Returns the time.
+     *
+     * @return string The time
+     */
+    public function getTime(): string
+    {
+        if ($this->time === null) {
+            return '0';
+        }
+
+        return $this->time;
+    }
+
+    /**
+     * [setTime description]
+     *
+     * @param string $time
+     *
+     * @return void
+     */
+    public function setTime(string $time): void
+    {
+        $this->time = $time;
+    }
+
+    /**
+     *
+     * @param string $statusCode
+     *
+     * @return void
+     */
+    public function setStatusCode(string $statusCode): void
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getStatusCode(): string
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * Gets a Collector by name.
+     *
+     * @param string $name A collector name
+     *
+     * @throws \InvalidArgumentException if the collector does not exist
+     *
+     * @return \Viserio\Contracts\WebProfiler\DataCollector
+     */
+    public function getCollector($name)
+    {
+        if (!isset($this->collectors[$name])) {
+            throw new InvalidArgumentException(sprintf('Collector "%s" does not exist.', $name));
+        }
+
+        return $this->collectors[$name];
+    }
+
+    /**
+     * Gets the Collectors associated with this profile.
+     *
+     * @return \Viserio\Contracts\WebProfiler\DataCollector[]
+     */
+    public function getCollectors(): array
+    {
+        return $this->collectors;
+    }
+
+    /**
+     * Sets the Collectors associated with this profile.
+     *
+     * @param \Viserio\Contracts\WebProfiler\DataCollector[] $collectors
+     */
+    public function setCollectors(array $collectors)
+    {
+        foreach ($collectors as $collector) {
+            $this->addCollector($collector);
+        }
+    }
+
+    /**
+     * Adds a Collector.
+     *
+     * @param \Viserio\Contracts\WebProfiler\DataCollector $collector
+     */
+    public function addCollector(DataCollectorContract $collector)
+    {
+        $this->collectors[$collector->getName()] = $collector;
+    }
+
+    /**
+     * Returns true if a Collector for the given name exists.
+     *
+     * @param string $name A collector name
+     *
+     * @return bool
+     */
+    public function hasCollector(string $name): bool
+    {
+        return isset($this->collectors[$name]);
+    }
+
+    /**
+     * [__sleep description]
+     *
+     * @return array
+     */
+    public function __sleep()
+    {
+        return [
+            'token',
+            'parent',
+            'children',
+            'collectors',
+            'ip',
+            'method',
+            'url',
+            'time',
+            'statusCode'
+        ];
+    }
 }
