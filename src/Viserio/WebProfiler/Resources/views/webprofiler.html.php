7865ea2b52a879a40dc877e5a0e7bb54c92e3956
Daniel Bannert <daniel.bannert@trnd.com>
2016-12-16 12:06:02 +0100

    use short array

--- src/Viserio/WebProfiler/Resources/views/webprofiler.html.php
+++ src/Viserio/WebProfiler/Resources/views/webprofiler.html.php
@@ -10,43 +10,35 @@ if (! isset($token, $menus, $icons, $panels)) {
         <?php echo file_get_contents($icons['ic_narrowspark_white_24px.svg']); ?>
     </a>
     <div class="webprofiler-header">
-        <?php
-            if (count($menus) !== 0) {
-                ?>
+        <?php if (count($menus) !== 0): ?>
         <div class="webprofiler-menus">
             <a class="webprofiler-hide-button" title="Close WebProfiler" tabindex="-1" accesskey="D">
                 <?php echo file_get_contents($icons['ic_clear_white_24px.svg']); ?>
             </a>
-        <?php
-                foreach ($menus as $name => $menu) {
-                    $tooltip = false;
+            <?php foreach ($menus as $name => $menu):
+                $tooltip = false;
 
-                    if (isset($menu['tooltip'])) {
-                        $tooltip = true;
-                    }
+                if (isset($menu['tooltip'])) {
+                    $tooltip = true;
+                }
 
-                    $data = isset($panels[$name]) ? 'data-panel-target-id="webprofiler-panel-' . $name . '"' : '';
-                    $hasPanels = isset($panels[$name]) ? ' webprofiler-menu-has-panel' : '';
-                    $hasTooltip = $tooltip ? ' webprofiler-menu-has-tooltip' : '';
-                    $cssClasses = isset($menu['menu']['class']) ? ' ' . $menu['menu']['class'] : ''; ?>
+                $data = isset($panels[$name]) ? 'data-panel-target-id="webprofiler-panel-' . $name . '"' : '';
+                $hasPanels = isset($panels[$name]) ? ' webprofiler-menu-has-panel' : '';
+                $hasTooltip = $tooltip ? ' webprofiler-menu-has-tooltip' : '';
+                $cssClasses = isset($menu['menu']['class']) ? ' ' . $menu['menu']['class'] : '';
+            ?>
             <a <?php echo TemplateHelper::escape($data) ?> class="webprofiler-menu webprofiler-menu-<?php echo TemplateHelper::escape($name) ?> webprofiler-menu-position-<?php echo TemplateHelper::escape($menu['position']) . TemplateHelper::escape($hasPanels) . TemplateHelper::escape($hasTooltip) . TemplateHelper::escape($cssClasses); ?>">
                 <div class="webprofiler-menu-content">
-                    <?php if (isset($menu['menu']['icon'])) {
-                        ?>
+                    <?php if (isset($menu['menu']['icon'])): ?>
                     <span class="webprofiler-menu-icon">
                         <?php echo isset($icons[$menu['menu']['icon']]) ? file_get_contents($icons[$menu['menu']['icon']]) : $menu['menu']['icon'] ?>
                     </span>
-                    <?php
-
-                    } ?>
-                    <?php if (isset($menu['menu']['status'])) {
-                        ?>
+                    <?php endif; ?>
+                    <?php if (isset($menu['menu']['status'])): ?>
                     <span class="webprofiler-menu-status">
                         <?php echo TemplateHelper::escape($menu['menu']['status']) ?>
                     </span>
-                    <?php
-
-                    } ?>
+                    <?php endif; ?>
                     <span class="webprofiler-menu-label">
                         <?php echo TemplateHelper::escape($menu['menu']['label']) ?>
                     </span>
@@ -54,36 +46,22 @@ if (! isset($token, $menus, $icons, $panels)) {
                         <?php echo TemplateHelper::escape($menu['menu']['value']) ?>
                     </span>
                 </div>
-                <?php
-                    if ($tooltip) {
-                        ?>
+                <?php if ($tooltip): ?>
                     <div class="webprofiler-menu-tooltip">
                         <?php echo TemplateHelper::escape($menu['tooltip']) ?>
                     </div>
-                <?php
-
-                    } ?>
+                <?php endif; ?>
             </a>
-        <?php
-
-                } ?>
+            <?php endforeach; ?>
         </div>
-        <?php
-
-            }
-        ?>
+        <?php endif; ?>
     </div>
     <div class="webprofiler-body">
         <div id="webprofiler-body-dragbar"></div>
-        <?php
-            foreach ($panels as $name => $panel) {
-                ?>
+        <?php foreach ($panels as $name => $panel): ?>
         <div class="webprofiler-panel webprofiler-panel-<?php echo TemplateHelper::escape($name) . TemplateHelper::escape($panel['class']); ?>">
             <?php echo TemplateHelper::escape($panel['content']) ?>
         </div>
-        <?php
-
-            }
-        ?>
+        <?php endforeach; ?>
     </div>
 </div>
