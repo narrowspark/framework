2288baa623d69f7c5e32c51de43b7b139784f360
Daniel Bannert <d.bannert@anolilab.de>
2016-12-29 00:07:23 +0000

    Apply fixes from StyleCI

--- src/Viserio/View/Tests/Fixture/bar/foo/fi.php
+++ src/Viserio/View/Tests/Fixture/bar/foo/fi.php
@@ -1,3 +1,3 @@
 <?php declare(strict_types=1);
-print $__env->create('layout', \Narrowspark\Arr\StaticArr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+echo $__env->create('layout', \Narrowspark\Arr\StaticArr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 <?php throw new Exception('section exception message'); ?>
