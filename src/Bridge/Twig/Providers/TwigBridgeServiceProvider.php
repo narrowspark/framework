ed1946f74c87d92e875ba22c6df0d581d3303df4
Daniel Bannert <d.bannert@anolilab.de>
2017-01-16 21:58:11 +0100

    new classes

---
+++ src/Bridge/Twig/Providers/TwigBridgeServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Bridge\Twig;
+
+use Interop\Container\ServiceProvider;
+
+class TwigBridgeServiceProvider implements ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getServices()
+    {
+        return [
+        ];
+    }
+}
