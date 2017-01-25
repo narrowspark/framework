956838a05c60bceb991a51043ab3558698d591fe
Daniel Bannert <d.bannert@anolilab.de>
2017-01-25 22:09:17 +0000

    Apply fixes from StyleCI

--- src/Viserio/Bridge/Twig/Tests/Providers/TwigBridgeCommandsServiceProviderTest.php
+++ src/Viserio/Bridge/Twig/Tests/Providers/TwigBridgeCommandsServiceProviderTest.php
@@ -3,11 +3,11 @@ declare(strict_types=1);
 namespace Viserio\Bridge\Twig\Tests\Providers;
 
 use PHPUnit\Framework\TestCase;
-use Viserio\Component\Container\Container;
 use Viserio\Bridge\Twig\Commands\CleanCommand;
 use Viserio\Bridge\Twig\Commands\DebugCommand;
 use Viserio\Bridge\Twig\Commands\LintCommand;
 use Viserio\Bridge\Twig\Providers\TwigBridgeCommandsServiceProvider;
+use Viserio\Component\Container\Container;
 
 class TwigBridgeCommandsServiceProviderTest extends TestCase
 {
