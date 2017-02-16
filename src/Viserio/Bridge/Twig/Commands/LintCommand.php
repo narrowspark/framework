6a66c12a8c959df9c29709172b315ccf54fce1d4
Daniel Bannert <d.bannert@anolilab.de>
2017-02-17 00:21:32 +0100

    added options resolver to lint command

--- src/Viserio/Bridge/Twig/Commands/LintCommand.php
+++ src/Viserio/Bridge/Twig/Commands/LintCommand.php
@@ -7,9 +7,14 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Finder\Finder;
 use Twig_Environment;
 use Viserio\Component\Console\Command\Command;
+use Viserio\Component\Contracts\OptionsResolver\RequiresComponentConfig as RequiresComponentConfigContract;
+use Viserio\Component\Contracts\OptionsResolver\RequiresMandatoryOptions as RequiresMandatoryOptionsContract;
+use Viserio\Component\OptionsResolver\Traits\ConfigurationTrait;
 
-class LintCommand extends Command
+class LintCommand extends Command implements RequiresComponentConfigContract, RequiresMandatoryOptionsContract
 {
+    use ConfigurationTrait;
+
     /**
      * {@inheritdoc}
      */
@@ -21,17 +26,42 @@ class LintCommand extends Command
     protected $description = 'Lints a template and outputs encountered errors';
 
     /**
+     * {@inheritdoc}
+     */
+    public function getDimensions(): iterable
+    {
+        return ['viserio', 'view'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMandatoryOptions(): iterable
+    {
+        return [
+            'paths',
+            'engines' => [
+                'twig' => [
+                    'options' => [
+                        'debug',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
      * Get a finder instance of Twig files in the specified directories.
      *
      * @param array $paths Paths to search for files in.
      *
      * @return \Symfony\Component\Finder\Finder
      */
-    public function getFinder(array $paths)
+    public function getFinder(array $paths): Finder
     {
         $finder = (empty($this->finder)) ? Finder::create() : $this->finder;
 
-        return $finder->files()->in($paths)->name('*.' . $this->laravel['twig.extension']);
+        return $finder->files()->in($paths)->name('*.' . $this->options['engines']['twig']['extension']);
     }
 
     /**
@@ -41,7 +71,7 @@ class LintCommand extends Command
      *
      * @return void
      */
-    public function setFinder(Finder $finder)
+    public function setFinder(Finder $finder): void
     {
         $this->finder = $finder;
     }
@@ -52,6 +82,8 @@ class LintCommand extends Command
     public function handle()
     {
         $container = $this->getContainer();
+
+        $this->configureOptions($container);
 
         if ($container->has(Twig_Environment::class)) {
             $this->error('The Twig environment needs to be set.');
