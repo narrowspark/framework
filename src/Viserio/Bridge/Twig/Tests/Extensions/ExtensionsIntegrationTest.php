c4e86382437e9f9ddf96568526ed218279ff4ea4
Daniel Bannert <daniel.bannert@trnd.com>
2017-02-16 11:32:00 +0100

    next appveyor fix

--- src/Viserio/Bridge/Twig/Tests/Extensions/ExtensionsIntegrationTest.php
+++ src/Viserio/Bridge/Twig/Tests/Extensions/ExtensionsIntegrationTest.php
@@ -18,6 +18,9 @@ if (! class_exists('\PHPUnit_Framework_TestCase')) {
     class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
 }
 
+/**
+ * @group appveyor
+ */
 class ExtensionsIntegrationTest extends Twig_Test_IntegrationTestCase
 {
     use MockeryTrait;
