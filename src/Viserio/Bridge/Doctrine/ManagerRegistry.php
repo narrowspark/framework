b8d2aac9b9c0d219fba3a097e4969640645cc3a8
Daniel Bannert <d.bannert@anolilab.de>
2017-02-19 23:06:31 +0100

    working on orm

---
+++ src/Viserio/Bridge/Doctrine/ManagerRegistry.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Bridge\Doctrine;
+
+use Doctrine\Common\Persistence\ManagerRegistry as BaseManagerRegistry;
+use Doctrine\Common\Persistence\Proxy;
+use Doctrine\ORM\ORMException;
+use InvalidArgumentException;
+use ReflectionClass;
+use Viserio\Component\Contracts\Container\Traits\ContainerAwareTrait;
+use Interop\Container\ContainerInterface;
+
+final class ManagerRegistry implements BaseManagerRegistry
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @const
+     */
+    const MANAGER_BINDING_PREFIX = 'doctrine.managers.';
+
+    /**
+     * @const
+     */
+    const CONNECTION_BINDING_PREFIX = 'doctrine.connections.';
+
+    /**
+     * @var string
+     */
+    protected $defaultManager = 'default';
+
+    /**
+     * @var string
+     */
+    protected $defaultConnection = 'default';
+
+    /**
+     * @var \Viserio\Bridge\Doctrine\EntityManagerFactory
+     */
+    protected $factory;
+
+    /**
+     * @var array
+     */
+    protected $managers = [];
+
+    /**
+     * @var array
+     */
+    protected $connections = [];
+
+    /**
+     * @var array
+     */
+    protected $managersMap = [];
+
+    /**
+     * @var array
+     */
+    protected $connectionsMap = [];
+
+    /**
+     * Create a new manager registry.
+     *
+     * @param \Interop\Container\ContainerInterface         $container
+     * @param \Viserio\Bridge\Doctrine\EntityManagerFactory $factory
+     */
+    public function __construct(ContainerInterface $container, EntityManagerFactory $factory)
+    {
+        $this->container = $container;
+        $this->factory   = $factory;
+    }
+}
