2a1dd0bb9b6744d302f47cfb5a36ff836e48d963
Daniel Bannert <d.bannert@anolilab.de>
2017-03-02 00:04:45 +0100

    added some tests

---
+++ src/Viserio/Component/Routing/Tests/Providers/RoutingDataCollectorServiceProviderTest.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Component\Routing\Tests\Providers;
+
+use Narrowspark\TestingHelper\Phpunit\MockeryTestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use Viserio\Component\Config\Providers\ConfigServiceProvider;
+use Viserio\Component\Container\Container;
+use Viserio\Component\Contracts\Config\Repository as RepositoryContract;
+use Viserio\Component\Contracts\Routing\Route as RouteContract;
+use Viserio\Component\Contracts\Routing\Router as RouterContract;
+use Viserio\Component\Contracts\WebProfiler\WebProfiler as WebProfilerContract;
+use Viserio\Component\Routing\Providers\RoutingDataCollectorServiceProvider;
+use Viserio\Component\HttpFactory\Providers\HttpFactoryServiceProvider;
+use Viserio\Component\OptionsResolver\Providers\OptionsResolverServiceProvider;
+use Viserio\Component\WebProfiler\Providers\WebProfilerServiceProvider;
+use Viserio\Component\Contracts\Routing\RouteCollection as RouteCollectionContract;
+
+class RoutingDataCollectorServiceProviderTest extends MockeryTestCase
+{
+    public function testGetServices()
+    {
+        $routes = $this->mock(RouteCollectionContract::class);
+        $router = $this->mock(RouterContract::class);
+        $router->shouldReceive('getRoutes')
+            ->once()
+            ->andReturn($routes);
+        $router->shouldReceive('group')
+            ->once();
+
+        $container = new Container();
+        $container->instance(ServerRequestInterface::class, $this->getRequest());
+        $container->instance(RouterContract::class, $router);
+        $container->register(new OptionsResolverServiceProvider());
+        $container->register(new HttpFactoryServiceProvider());
+        $container->register(new WebProfilerServiceProvider());
+        $container->register(new RoutingDataCollectorServiceProvider());
+
+        $container->instance('config',
+            [
+                'viserio' => [
+                    'webprofiler' => [
+                        'enable'    => true,
+                        'collector' => [
+                            'routes'  => true,
+                        ],
+                    ],
+                ]
+            ]
+        );
+
+        $profiler = $container->get(WebProfilerContract::class);
+
+        static::assertInstanceOf(WebProfilerContract::class, $profiler);
+
+        static::assertTrue(array_key_exists('time-data-collector', $profiler->getCollectors()));
+        static::assertTrue(array_key_exists('memory-data-collector', $profiler->getCollectors()));
+        static::assertTrue(array_key_exists('routing-data-collector', $profiler->getCollectors()));
+    }
+
+    private function getRequest()
+    {
+        $request = $this->mock(ServerRequestInterface::class);
+        $request->shouldReceive('getHeaderLine')
+            ->with('REQUEST_TIME_FLOAT')
+            ->andReturn(false);
+        $request->shouldReceive('getHeaderLine')
+            ->with('REQUEST_TIME')
+            ->andReturn(false);
+
+        return $request;
+    }
+}
--- src/Viserio/Component/Routing/Tests/Providers/RoutingServiceProviderTest.php
+++ src/Viserio/Component/Routing/Tests/Providers/RoutingServiceProviderTest.php
@@ -7,16 +7,25 @@ use Viserio\Component\Container\Container;
 use Viserio\Component\Events\Providers\EventsServiceProvider;
 use Viserio\Component\Routing\Providers\RoutingServiceProvider;
 use Viserio\Component\Routing\Router;
+use Viserio\Component\Routing\UrlGenerator;
+use Viserio\Component\Contracts\Routing\UrlGenerator as UrlGeneratorContract;
+use Psr\Http\Message\ServerRequestInterface;
+use Interop\Http\Factory\UriFactoryInterface;
+use Narrowspark\TestingHelper\Phpunit\MockeryTestCase;
 
-class RoutingServiceProviderTest extends TestCase
+class RoutingServiceProviderTest extends MockeryTestCase
 {
     public function testProvider()
     {
         $container = new Container();
         $container->register(new RoutingServiceProvider());
         $container->register(new EventsServiceProvider());
+        $container->instance(ServerRequestInterface::class, $this->mock(ServerRequestInterface::class));
+        $container->instance(UriFactoryInterface::class, $this->mock(UriFactoryInterface::class));
 
         self::assertInstanceOf(Router::class, $container->get(Router::class));
+        self::assertInstanceOf(UrlGeneratorContract::class, $container->get(UrlGeneratorContract::class));
+        self::assertInstanceOf(UrlGeneratorContract::class, $container->get(UrlGenerator::class));
         self::assertInstanceOf(Router::class, $container->get('router'));
     }
 }
