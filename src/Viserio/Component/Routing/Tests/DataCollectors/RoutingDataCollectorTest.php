ab3932bfda690431831f539e0f6d6834b88c3cc4
Daniel Bannert <daniel.bannert@trnd.com>
2017-03-03 14:52:29 +0100

    more tests

--- src/Viserio/Component/Routing/Tests/DataCollectors/RoutingDataCollectorTest.php
+++ src/Viserio/Component/Routing/Tests/DataCollectors/RoutingDataCollectorTest.php
@@ -32,4 +32,46 @@ class RoutingDataCollectorTest extends MockeryTestCase
             'counted' => 1,
         ], $data);
     }
+
+    public function testGetMenu()
+    {
+        $route  = new Route('GET', '/test', ['domain' => 'test.com']);
+        $routes = $this->mock(RouteCollectionContract::class);
+        $routes->shouldReceive('getRoutes')
+            ->twice()
+            ->andReturn([$route]);
+        $collector = new RoutingDataCollector($routes);
+
+        $collector->collect(
+            $this->mock(ServerRequestInterface::class),
+            $this->mock(ResponseInterface::class)
+        );
+
+        $data = $collector->getData();
+
+        self::assertSame([
+            'icon'  => file_get_contents(__DIR__ . '/../../DataCollectors/Resources/icons/ic_directions_white_24px.svg'),
+            'label' => 'Routes',
+            'value' => 1,
+        ], $collector->getMenu());
+    }
+
+    public function testGetPanel()
+    {
+        $route  = new Route('GET', '/test', ['domain' => 'test.com']);
+        $routes = $this->mock(RouteCollectionContract::class);
+        $routes->shouldReceive('getRoutes')
+            ->twice()
+            ->andReturn([$route]);
+        $collector = new RoutingDataCollector($routes);
+
+        $collector->collect(
+            $this->mock(ServerRequestInterface::class),
+            $this->mock(ResponseInterface::class)
+        );
+
+        $data = $collector->getData();
+
+        self::assertSame('<table class="row"><thead><tr><th scope="col" class="Methods">Methods</th><th scope="col" class="Path">Path</th><th scope="col" class="Name">Name</th><th scope="col" class="Action">Action</th><th scope="col" class="With Middleware">With Middleware</th><th scope="col" class="Without Middleware">Without Middleware</th><th scope="col" class="Domain">Domain</th></tr></thead><tbody><tr><td>GET | HEAD</td><td>/test</td><td></td><td>Closure</td><td></td><td></td><td>Domain</td></tr></tbody></table>', $collector->getPanel());
+    }
 }
