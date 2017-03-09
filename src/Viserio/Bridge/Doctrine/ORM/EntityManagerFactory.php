dc28d71357dac9684cfa44df387094b7eab64e5c
Daniel Bannert <d.bannert@anolilab.de>
2017-03-10 00:52:15 +0100

    using component cache with a bridge

--- src/Viserio/Bridge/Doctrine/ORM/EntityManagerFactory.php
+++ src/Viserio/Bridge/Doctrine/ORM/EntityManagerFactory.php
@@ -2,15 +2,25 @@
 declare(strict_types=1);
 namespace Viserio\Bridge\Doctrine\ORM;
 
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Tools\Setup;
 use Interop\Container\ContainerInterface;
 use Viserio\Component\Contracts\Container\Traits\ContainerAwareTrait;
+use Viserio\Component\Contracts\Cache\Manager as CacheManagerContract;
+use Viserio\Bridge\Doctrine\ORM\Resolvers\EntityListenerResolver;
+use Viserio\Bridge\Doctrine\ORM\Configuration\MetaData\MetaDataManager;
+use Viserio\Component\OptionsResolver\Traits\ConfigurationTrait;
 
 class EntityManagerFactory
 {
     use ContainerAwareTrait;
+    use ConfigurationTrait;
 
     /**
-     * @var MetaDataManager
+     * @var \Viserio\Bridge\Doctrine\ORM\Configuration\MetaData\MetaDataManager
      */
     protected $meta;
 
@@ -20,7 +30,7 @@ class EntityManagerFactory
     protected $connection;
 
     /**
-     * @var CacheManager
+     * @var \Viserio\Component\Contracts\Cache\Manager
      */
     protected $cache;
 
@@ -39,8 +49,14 @@ class EntityManagerFactory
      *
      * @param \Interop\Container\ContainerInterface $container
      */
-    public function __construct(ContainerInterface $container)
-    {
+    public function __construct(
+        ContainerInterface $container,
+        Setup $setup,
+        MetaDataManager $meta,
+        CacheManagerContract $cache,
+        EntityListenerResolver $resolver
+    ) {
         $this->container = $container;
-    }
+
+        $this->configureOptions($this->container);
 }
