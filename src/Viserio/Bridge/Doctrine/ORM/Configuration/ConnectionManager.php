dfe58781b847cc08dadc6450ebae720fd1221bc2
Daniel Bannert <d.bannert@anolilab.de>
2017-03-12 20:23:23 +0100

    more work

---
+++ src/Viserio/Bridge/Doctrine/ORM/Configuration/ConnectionManager.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Bridge\Doctrine\ORM\Configuration;
+
+use Viserio\Component\Support\AbstractConnectionManager;
+
+class ConnectionManager extends AbstractConnectionManager
+{
+}
