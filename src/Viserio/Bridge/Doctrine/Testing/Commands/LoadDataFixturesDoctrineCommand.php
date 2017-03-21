ed2bb77be31fc100ff9dfde19f673a9daa0dad9a
Daniel Bannert <daniel.bannert@trnd.com>
2017-03-21 14:18:39 +0100

    start adding data-fixtures

---
+++ src/Viserio/Bridge/Doctrine/Testing/Commands/LoadDataFixturesDoctrineCommand.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Bridge\Doctrine\Testing\Commands;
+
+class LoadDataFixturesDoctrineCommand
+{
+
+}
---
+++ src/Viserio/Bridge/Doctrine/Testing/Providers/ConsoleCommandsServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Bridge\Doctrine\Testing\Providers;
+
+use Viserio\Bridge\Doctrine\Testing\Commands\LoadDataFixturesDoctrineCommand;
+use Interop\Container\ContainerInterface;
+use Interop\Container\ServiceProvider;
+use Viserio\Component\Contracts\Console\Application as ApplicationContract;
+
+class ConsoleCommandsServiceProvider implements ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getServices()
+    {
+        return [
+            ApplicationContract::class => [self::class, 'createConsoleCommands'],
+        ];
+    }
+
+    /**
+     * Extend viserio console with new command.
+     *
+     * @param \Interop\Container\ContainerInterface $container
+     * @param null|callable                         $getPrevious
+     *
+     * @return null|\Viserio\Component\Contracts\Console\Application
+     */
+    public static function createConsoleCommands(ContainerInterface $container, ?callable $getPrevious = null): ?ApplicationContract
+    {
+        if ($getPrevious !== null) {
+            $console = $getPrevious();
+
+            $console->add(new LoadDataFixturesDoctrineCommand());
+
+            return $console;
+        }
+
+        return null;
+    }
+}
