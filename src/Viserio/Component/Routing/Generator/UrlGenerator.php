c5f286fb25e16766038f634306a5d555a20d9d29
Daniel Bannert <prisis33@gmail.com>
2017-03-22 23:37:06 +0000

    Apply fixes from StyleCI

--- src/Viserio/Component/Routing/Generator/UrlGenerator.php
+++ src/Viserio/Component/Routing/Generator/UrlGenerator.php
@@ -6,13 +6,13 @@ use Interop\Http\Factory\UriFactoryInterface;
 use Narrowspark\Arr\Arr;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriInterface;
+use Viserio\Component\Contracts\Log\Traits\LoggerAwareTrait;
 use Viserio\Component\Contracts\Routing\Exceptions\RouteNotFoundException;
 use Viserio\Component\Contracts\Routing\Exceptions\UrlGenerationException;
 use Viserio\Component\Contracts\Routing\Route as RouteContract;
 use Viserio\Component\Contracts\Routing\RouteCollection as RouteCollectionContract;
 use Viserio\Component\Contracts\Routing\UrlGenerator as UrlGeneratorContract;
 use Viserio\Component\Support\Traits\MacroableTrait;
-use Viserio\Component\Contracts\Log\Traits\LoggerAwareTrait;
 
 class UrlGenerator implements UrlGeneratorContract
 {
