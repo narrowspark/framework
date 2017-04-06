440f38b2dca0fa24efd14861a5442305b013423f
Daniel Bannert <daniel.bannert@trnd.com>
2017-04-06 13:38:20 +0200

    remove key

--- build/travis/after_success.sh
+++ build/travis/after_success.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [["$SEND_COVERAGE" = true ]]; then
-    bash <(curl -s https://codecov.io/bash) -t 1e319602-740e-4a6f-b25f-f4b52f62760d
+    bash <(curl -s https://codecov.io/bash)
 fi
