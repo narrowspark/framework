6eb35666fd3a9d76d78a00a0fd8ad7d269e64d0e
Daniel Bannert <d.bannert@anolilab.de>
2017-04-10 00:21:43 +0200

    use Swift_Mime_Message

--- src/Viserio/Component/Mail/Events/MessageSendingEvent.php
+++ src/Viserio/Component/Mail/Events/MessageSendingEvent.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 namespace Viserio\Component\Mail\Events;
 
+use Swift_Mime_Message;
 use Viserio\Component\Contracts\Events\Event as EventContract;
 use Viserio\Component\Contracts\Mail\Mailer as MailerContract;
 use Viserio\Component\Events\Traits\EventTrait;
@@ -14,12 +15,22 @@ class MessageSendingEvent implements EventContract
      * Create a new message sending event.
      *
      * @param \Viserio\Component\Contracts\Mail\Mailer $mailer
-     * @param array                                    $param
+     * @param \Swift_Mime_Message                      $param
      */
-    public function __construct(MailerContract $mailer, array $param)
+    public function __construct(MailerContract $mailer, Swift_Mime_Message $message)
     {
         $this->name       = 'message.sending';
         $this->target     = $mailer;
-        $this->parameters = $param;
+        $this->parameters = ['message' => $message];
+    }
+
+    /**
+     * Get swift message.
+     *
+     * @return \Swift_Mime_Message
+     */
+    public function getMessage(): Swift_Mime_Message
+    {
+        return $this->parameters['message'];
     }
 }
