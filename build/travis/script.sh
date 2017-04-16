0a8c2279fcc7a65539853621d09dcae5de426718
Daniel Bannert <d.bannert@anolilab.de>
2017-04-16 13:48:13 +0200

    change psalm

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -20,6 +20,4 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-if [[ "$PSALM" = true ]]; then
-    ./vendor/bin/psalm
-fi
+if [[ "$PSALM" = true ]]; then ./vendor/bin/psalm -c psalm.xml --find-dead-code; fi
