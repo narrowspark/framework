3be41f7687b8894c8a1823f52cb96758398957a3
Daniel Bannert <daniel.bannert@trnd.com>
2017-04-27 12:45:03 +0200

    fix tests

--- src/Viserio/Component/Foundation/Tests/DataCollectors/ViserioHttpDataCollectorTest.php
+++ src/Viserio/Component/Foundation/Tests/DataCollectors/ViserioHttpDataCollectorTest.php
@@ -2,57 +2,53 @@
 declare(strict_types=1);
 namespace Viserio\Component\Foundation\Tests\DataCollectors;
 
-// use Narrowspark\TestingHelper\Phpunit\MockeryTestCase;
-// use Psr\Http\Message\ResponseInterface;
-// use Psr\Http\Message\ServerRequestInterface;
-// use Viserio\Component\Contracts\Config\Repository as RepositoryContract;
-// use Viserio\Component\Contracts\Routing\Route as RouteContract;
-// use Viserio\Component\Contracts\Routing\Router as RouterContract;
-// use Viserio\Component\Foundation\DataCollectors\ViserioHttpDataCollector;
+use Narrowspark\TestingHelper\Phpunit\MockeryTestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Viserio\Component\Contracts\Config\Repository as RepositoryContract;
+use Viserio\Component\Contracts\Routing\Route as RouteContract;
+use Viserio\Component\Contracts\Routing\Router as RouterContract;
+use Viserio\Component\Foundation\DataCollectors\ViserioHttpDataCollector;
 
-// class ViserioHttpDataCollectorTest extends MockeryTestCase
-// {
-//     public function testGetMenuAndPosition()
-//     {
-//         $serverRequest = $this->mock(ServerRequestInterface::class);
-//         $serverRequest->shouldReceive('getAttributes')
-//             ->once()
-//             ->withNoArgs()
-//             ->andReturn([]);
+class ViserioHttpDataCollectorTest extends MockeryTestCase
+{
+    public function testGetMenuAndPosition()
+    {
+        $serverRequest = $this->mock(ServerRequestInterface::class);
+        $serverRequest->shouldReceive('getAttributes')
+            ->once()
+            ->withNoArgs()
+            ->andReturn([]);
 
-//         $response = $this->mock(ResponseInterface::class);
-//         $response->shouldReceive('getStatusCode')
-//             ->once()
-//             ->withNoArgs()
-//             ->andReturn(200);
+        $response = $this->mock(ResponseInterface::class);
+        $response->shouldReceive('getStatusCode')
+            ->once()
+            ->withNoArgs()
+            ->andReturn(200);
 
-//         $route = $this->mock(RouteContract::class);
-//         $route->shouldReceive('getServerRequest')
-//             ->once()
-//             ->withNoArgs()
-//             ->andReturn($serverRequest);
-//         $route->shouldReceive('getName')
-//             ->twice()
-//             ->withNoArgs()
-//             ->andReturn('Home');
+        $route = $this->mock(RouteContract::class);
+        $route->shouldReceive('getName')
+            ->twice()
+            ->withNoArgs()
+            ->andReturn('Home');
 
-//         $router = $this->mock(RouterContract::class);
-//         $router->shouldReceive('getCurrentRoute')
-//             ->once()
-//             ->withNoArgs()
-//             ->andReturn($route);
+        $router = $this->mock(RouterContract::class);
+        $router->shouldReceive('getCurrentRoute')
+            ->once()
+            ->withNoArgs()
+            ->andReturn($route);
 
-//         $collect = new ViserioHttpDataCollector($router, $this->mock(RepositoryContract::class));
-//         $collect->collect($this->mock(ServerRequestInterface::class), $response);
+        $collect = new ViserioHttpDataCollector($router, $this->mock(RepositoryContract::class));
+        $collect->collect($serverRequest, $response);
 
-//         static::assertSame(
-//             [
-//                 'label' => '@',
-//                 'value' => 'Home',
-//             ],
-//             $collect->getMenu()
-//         );
+        static::assertSame(
+            [
+                'label' => '@',
+                'value' => 'Home',
+            ],
+            $collect->getMenu()
+        );
 
-//         static::assertSame('left', $collect->getMenuPosition());
-//     }
-// }
+        static::assertSame('left', $collect->getMenuPosition());
+    }
+}
