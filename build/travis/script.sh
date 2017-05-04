c5b08b2e94ecdee7f3651787831c89f54fcb7c94
Daniel Bannert <d.bannert@anolilab.de>
2017-05-04 20:27:17 +0200

    try

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -15,31 +15,21 @@ tfold () {
 }
 export -f tfold
 
-for f in ./src/Viserio/*/*; do
-    if [[ -d "$f" && ! -L "$f" ]]; then
-        SLUG="$(basename $f)";
-        TYPE="$(basename ${f%/*})";
+if [[ "$PHPUNIT" = true && "$SEND_COVERAGE" = true ]]; then
+    ./vendor/bin/phpunit -c phpunit.xml.dist --verbose --coverage-clover=coverage.xml;
+elif [[ "$PHPUNIT" = true ]]; then
+    for f in ./src/Viserio/*/*; do
+        if [[ -d "$f" && ! -L "$f" ]]; then
+            SLUG="$(basename $f)";
+            TYPE="$(basename ${f%/*})";
 
-        if [[ "$TYPE" = "Component" ]]; then
-            tfold ./vendor/bin/phpunit -c phpunit.xml.dist --testsuite="Narrowspark $SLUG Component Test Suite" --verbose;
-        elif [[ "$TYPE" = "Bridge" ]]; then
-            tfold ./vendor/bin/phpunit -c phpunit.xml.dist --testsuite="Narrowspark $SLUG Bridge Test Suite" --verbose;
+            if [[ "$TYPE" = "Component" ]]; then
+                tfold ./vendor/bin/phpunit -c phpunit.xml.dist --testsuite="Narrowspark $SLUG Component Test Suite" --verbose;
+            elif [[ "$TYPE" = "Bridge" ]]; then
+                tfold ./vendor/bin/phpunit -c phpunit.xml.dist --testsuite="Narrowspark $SLUG Bridge Test Suite" --verbose;
+            fi
         fi
-    fi
-done
-
-set +e
-bash -e <<TRY
-    if [[ "$PHPSTAN" = true ]]; then
-        ./vendor/bin/phpstan analyse -c phpstan.neon -l 5 src/Viserio
-    fi
-
-    if [[ "$PHPUNIT" = true && "$SEND_COVERAGE" = true ]]; then
-        ./vendor/bin/phpunit -c phpunit.xml.dist --verbose --coverage-clover=coverage.xml;
-    elif [[ "$PHPUNIT" = true ]]; then
-
-    fi
-TRY
-if [ $? -ne 0 ]; then
-  exit 1
+    done
+elif [[ "$PHPSTAN" = true ]]; then
+    ./vendor/bin/phpstan analyse -c phpstan.neon -l 5 src/Viserio
 fi
