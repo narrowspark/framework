e52276229cc289f938b2e5510350f07e277484f1
Daniel Bannert <d.bannert@anolilab.de>
2017-05-04 23:25:13 +0200

    try

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -4,32 +4,35 @@
 mkdir -p build/logs
 
 # tfold is a helper to create folded reports
+# Arguments:
+#   $1 fold name
+#   $2 command to execute
 tfold () {
     title=$1
     fold=$(echo $title | sed -r 's/[^-_A-Za-z\d]+/./g')
     shift
     echo -e "travis_fold:start:$fold\\n\\e[1;34m$title\\e[0m"
-    "$*" 2>&1 && echo -e "\\e[32mOK\\e[0m $title\\n\\ntravis_fold:end:$fold" || ( echo -e "\\e[41mKO\\e[0m $title\\n" && exit 1 )
+    bash -xc "$*" 2>&1 && echo -e "\\e[32mOK\\e[0m $title\\n\\ntravis_fold:end:$fold" || ( echo -e "\\e[41mKO\\e[0m $title\\n" && exit 1 )
 }
 export -f tfold
-
-if [[ "$PHPUNIT" = true && "$SEND_COVERAGE" = true ]]; then
-    ./vendor/bin/phpunit -c ./phpunit.xml.dist --verbose --coverage-clover=coverage.xml;
-elif [[ "$PHPUNIT" = true ]]; then
-    for f in ./src/Viserio/*/*; do
+TEST="phpunit"
+# if [[ "$PHPUNIT" = true && "$SEND_COVERAGE" = true ]]; then
+#     ./vendor/bin/phpunit -c ./phpunit.xml.dist --verbose --coverage-clover=coverage.xml;
+# elif [[ "$PHPUNIT" = true ]]; then
+    for f in ../../src/Viserio/*/*; do
         if [[ -d "$f" && ! -L "$f" ]]; then
             SLUG="$(basename $f)";
             TYPE="$(basename ${f%/*})";
 
             if [[ "$TYPE" = "Component" ]]; then
-                TESTSUITE="Narrowspark $SLUG Component Component Suite";
+                TESTSUITE="Narrowspark $SLUG Component Test Suite";
             elif [[ "$TYPE" = "Bridge" ]]; then
                 TESTSUITE="Narrowspark $SLUG Bridge Test Suite";
             fi
 
-            tfold "$TESTSUITE" "$("$TEST" -c ./phpunit.xml.dist --testsuite="$TESTSUITE")";
+            tfold "$TESTSUITE" "$TEST -c ../../phpunit.xml.dist --testsuite=\"$TESTSUITE\"";
         fi
     done
-elif [[ "$PHPSTAN" = true ]]; then
-    ./vendor/bin/phpstan analyse -c phpstan.neon -l 5 src/Viserio
-fi
+# elif [[ "$PHPSTAN" = true ]]; then
+#     ./vendor/bin/phpstan analyse -c phpstan.neon -l 5 src/Viserio
+# fi
