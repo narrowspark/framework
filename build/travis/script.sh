7d13cbf76d6260cffa61b7e542948795c1c831da
Daniel Bannert <d.bannert@anolilab.de>
2017-05-04 22:42:06 +0200

    try

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -6,7 +6,7 @@ mkdir -p build/logs
 # tfold is a helper to create folded reports
 tfold () {
     title=$1
-    fold=$(echo $2 | sed -r 's/[^-_A-Za-z\d]+/./g')
+    fold=$(echo $title | sed -r 's/[^-_A-Za-z\d]+/./g')
     shift
     echo -e "travis_fold:start:$fold\\n\\e[1;34m$title\\e[0m"
     bash -xc "$*" 2>&1 && echo -e "\\e[32mOK\\e[0m $title\\n\\ntravis_fold:end:$fold" || ( echo -e "\\e[41mKO\\e[0m $title\\n" && exit 1 )
