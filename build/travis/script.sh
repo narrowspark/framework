c4a31e30c3cf8a725fb0ebaaccd3c4bf42180f24
Daniel Bannert <d.bannert@anolilab.de>
2017-05-04 22:49:18 +0200

    try

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -9,12 +9,12 @@ tfold () {
     fold=$(echo $title | sed -r 's/[^-_A-Za-z\d]+/./g')
     shift
     echo -e "travis_fold:start:$fold\\n\\e[1;34m$title\\e[0m"
-    "$*" 2>&1 && echo -e "\\e[32mOK\\e[0m $title\\n\\ntravis_fold:end:$fold" || ( echo -e "\\e[41mKO\\e[0m $title\\n" && exit 1 )
+    "$2" 2>&1 && echo -e "\\e[32mOK\\e[0m $title\\n\\ntravis_fold:end:$fold" || ( echo -e "\\e[41mKO\\e[0m $title\\n" && exit 1 )
 }
 export -f tfold
 
 if [[ "$PHPUNIT" = true && "$SEND_COVERAGE" = true ]]; then
-    ./vendor/bin/phpunit -c phpunit.xml.dist --verbose --coverage-clover=coverage.xml;
+    ./vendor/bin/phpunit -c ./phpunit.xml.dist --verbose --coverage-clover=coverage.xml;
 elif [[ "$PHPUNIT" = true ]]; then
     for f in ./src/Viserio/*/*; do
         if [[ -d "$f" && ! -L "$f" ]]; then
@@ -22,7 +22,7 @@ elif [[ "$PHPUNIT" = true ]]; then
             TYPE="$(basename ${f%/*})";
 
             if [[ "$TYPE" = "Component" ]]; then
-                TESTSUITE="Narrowspark $SLUG Bridge Component Suite";
+                TESTSUITE="Narrowspark $SLUG Component Component Suite";
             elif [[ "$TYPE" = "Bridge" ]]; then
                 TESTSUITE="Narrowspark $SLUG Bridge Test Suite";
             fi
