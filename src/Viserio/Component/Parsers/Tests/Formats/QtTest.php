6baf4ca928bebf703de3e28f7c21139226d00a2d
Daniel Bannert <daniel.bannert@trnd.com>
2017-05-11 16:06:24 +0200

    fix test

--- src/Viserio/Component/Parsers/Tests/Formats/QtTest.php
+++ src/Viserio/Component/Parsers/Tests/Formats/QtTest.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 namespace Viserio\Component\Parsers\Tests\Formats;
 
 use PHPUnit\Framework\TestCase;
+use Viserio\Component\Filesystem\Filesystem;
+use Viserio\Component\Parsers\Formats\Qt;
 
 class QtTest extends TestCase
 {
