c399b00db0c87b716b69c034e85f8f87e337ded3
Daniel Bannert <daniel.bannert@trnd.com>
2017-05-16 16:34:59 +0200

    fix IntlMessageFormatterTest

--- src/Viserio/Component/Translation/Tests/Formatters/IntlMessageFormatterTest.php
+++ src/Viserio/Component/Translation/Tests/Formatters/IntlMessageFormatterTest.php
@@ -23,7 +23,23 @@ class IntlMessageFormatterTest extends TestCase
      */
     public function testFormat($expected, $message, $arguments)
     {
-        $this->assertEquals($expected, trim((new IntlMessageFormatter())->format($message, 'en', $arguments)));
+        self::assertEquals($expected, trim((new IntlMessageFormatter())->format($message, 'en', $arguments)));
+    }
+
+    public function provideDataForFormat()
+    {
+        return [
+            [
+                'There is one apple',
+                'There is one apple',
+                [],
+            ],
+            [
+                '4,560 monkeys on 123 trees make 37.073 monkeys per tree',
+                '{0,number,integer} monkeys on {1,number,integer} trees make {2,number} monkeys per tree',
+                [4560, 123, 4560 / 123],
+            ],
+        ];
     }
 
     public function testFormatWithNamedArguments()
@@ -50,29 +66,15 @@ class IntlMessageFormatterTest extends TestCase
       =2 {{host} invites {guest} and one other person to their party.}
      other {{host} invites {guest} as one of the # people invited to their party.}}}}
 _MSG_;
-        $formatter = $this->getMessageFormatter();
+
+        $formatter = new IntlMessageFormatter();
         $message   = $formatter->format($chooseMessage, 'en', [
             'gender_of_host' => 'male',
             'num_guests'     => 10,
             'host'           => 'Fabien',
             'guest'          => 'Guilherme',
         ]);
-        $this->assertEquals('Fabien invites Guilherme as one of the 9 people invited to his party.', $message);
-    }
 
-    public function provideDataForFormat()
-    {
-        return [
-            [
-                'There is one apple',
-                'There is one apple',
-                [],
-            ],
-            [
-                '4,560 monkeys on 123 trees make 37.073 monkeys per tree',
-                '{0,number,integer} monkeys on {1,number,integer} trees make {2,number} monkeys per tree',
-                [4560, 123, 4560 / 123],
-            ],
-        ];
+        self::assertEquals('Fabien invites Guilherme as one of the 9 people invited to his party.', $message);
     }
 }
