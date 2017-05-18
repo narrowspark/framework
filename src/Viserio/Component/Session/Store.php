63223f811db280598417a956d2dd9d85c8e9fb1d
Daniel Bannert <d.bannert@anolilab.de>
2017-05-19 00:25:21 +0200

    cs fix

--- src/Viserio/Component/Session/Store.php
+++ src/Viserio/Component/Session/Store.php
@@ -55,7 +55,7 @@ class Store implements StoreContract
     protected $encrypter;
 
     /**
-     * Number of requests after which id is regeneratd.
+     * Number of requests after which id is regenerated.
      *
      * @var int|null
      */
