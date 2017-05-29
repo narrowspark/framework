ea0ba2bcdcecece3e95b08df35ce3a3cf28ec320
Daniel Bannert <daniel.bannert@trnd.com>
2017-05-29 14:34:41 +0200

    start working on #471 and refactor code

---
+++ src/Viserio/Component/OptionsResolver/Commands/ConfigReaderCommand.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Component\OptionsResolver\Commands;
+
+use Viserio\Component\Console\Command\Command;
+
+class ConfigReaderCommand extends Command
+{
+
+}
