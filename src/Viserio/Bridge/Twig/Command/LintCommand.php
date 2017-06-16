d7769b7b37215a59afdb6a6dfe707800aeb1ce0e
Daniel Bannert <daniel.bannert@trnd.com>
2017-06-16 12:37:42 +0200

    check

--- src/Viserio/Bridge/Twig/Command/LintCommand.php
+++ src/Viserio/Bridge/Twig/Command/LintCommand.php
@@ -291,7 +291,7 @@ class LintCommand extends Command
         $exception = $info['exception'];
         $line      = $exception->getTemplateLine();
         $lines     = $this->getContext($info['template'], $line);
-
+\var_dump($info);
         $this->line(sprintf('<error>Fail</error> in %s (line %s)', $info['file'], $line));
 
         foreach ($lines as $no => $code) {
