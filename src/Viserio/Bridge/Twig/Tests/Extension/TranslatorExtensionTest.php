dddbc1b8fcfbb7ef4b3bd7a50034df2c27e38fbe
Daniel Bannert <prisis33@gmail.com>
2017-06-26 13:03:48 +0000

    Apply fixes from StyleCI

--- src/Viserio/Bridge/Twig/Tests/Extension/TranslatorExtensionTest.php
+++ src/Viserio/Bridge/Twig/Tests/Extension/TranslatorExtensionTest.php
@@ -65,18 +65,21 @@ class TranslatorExtensionTest extends MockeryTestCase
 
     /**
      * @dataProvider getTransTests
+     *
+     * @param mixed $template
+     * @param mixed $expected
      */
-    public function testTrans($template, $expected, array $variables = array())
+    public function testTrans($template, $expected, array $variables = [])
     {
         if ($expected != $this->getTemplate($template)->render($variables)) {
-            echo $template."\n";
+            echo $template . "\n";
 
-            $loader = new TwigArrayLoader(array('index' => $template));
-            $twig   = new Environment($loader, array('debug' => true, 'cache' => false));
+            $loader = new TwigArrayLoader(['index' => $template]);
+            $twig   = new Environment($loader, ['debug' => true, 'cache' => false]);
 
             $twig->addExtension(new TranslatorExtension($this->getTranslationManager()));
 
-            echo $twig->compile($twig->parse($twig->tokenize($twig->getLoader()->getSourceContext('index'))))."\n\n";
+            echo $twig->compile($twig->parse($twig->tokenize($twig->getLoader()->getSourceContext('index')))) . "\n\n";
 
             self::assertEquals($expected, $this->getTemplate($template)->render($variables));
         }
@@ -86,38 +89,38 @@ class TranslatorExtensionTest extends MockeryTestCase
 
     public function getTransTests()
     {
-        return array(
+        return [
             // trans tag
-            array('{% trans %}Hello{% endtrans %}', 'Hello'),
-            array('{% trans %}%name%{% endtrans %}', 'Symfony', array('name' => 'Symfony')),
-            array('{% trans from elsewhere %}Hello{% endtrans %}', 'Hello'),
-            array('{% trans %}Hello %name%{% endtrans %}', 'Hello Symfony', array('name' => 'Symfony')),
-            array('{% trans with { \'%name%\': \'Symfony\' } %}Hello %name%{% endtrans %}', 'Hello Symfony'),
-            array('{% set vars = { \'%name%\': \'Symfony\' } %}{% trans with vars %}Hello %name%{% endtrans %}', 'Hello Symfony'),
-            array('{% trans into "fr"%}Hello{% endtrans %}', 'Hello'),
-            array('{% trans count from "messages" %}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples{% endtrans %}',
-                'There is no apples', array('count' => 0)),
-            array('{% trans count %}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples{% endtrans %}',
-                'There is 5 apples', array('count' => 5)),
-            array('{% trans count %}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples (%name%){% endtrans %}',
-                'There is 5 apples (Symfony)', array('count' => 5, 'name' => 'Symfony')),
-            array('{% trans count with { \'%name%\': \'Symfony\' } %}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples (%name%){% endtrans %}',
-                'There is 5 apples (Symfony)', array('count' => 5)),
-            array('{% trans count into "fr"%}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples{% endtrans %}',
-                'There is no apples', array('count' => 0)),
-            array('{% trans 5 into "fr"%}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples{% endtrans %}',
-                'There is 5 apples'),
+            ['{% trans %}Hello{% endtrans %}', 'Hello'],
+            ['{% trans %}%name%{% endtrans %}', 'Symfony', ['name' => 'Symfony']],
+            ['{% trans from elsewhere %}Hello{% endtrans %}', 'Hello'],
+            ['{% trans %}Hello %name%{% endtrans %}', 'Hello Symfony', ['name' => 'Symfony']],
+            ['{% trans with { \'%name%\': \'Symfony\' } %}Hello %name%{% endtrans %}', 'Hello Symfony'],
+            ['{% set vars = { \'%name%\': \'Symfony\' } %}{% trans with vars %}Hello %name%{% endtrans %}', 'Hello Symfony'],
+            ['{% trans into "fr"%}Hello{% endtrans %}', 'Hello'],
+            ['{% trans count from "messages" %}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples{% endtrans %}',
+                'There is no apples', ['count' => 0], ],
+            ['{% trans count %}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples{% endtrans %}',
+                'There is 5 apples', ['count' => 5], ],
+            ['{% trans count %}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples (%name%){% endtrans %}',
+                'There is 5 apples (Symfony)', ['count' => 5, 'name' => 'Symfony'], ],
+            ['{% trans count with { \'%name%\': \'Symfony\' } %}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples (%name%){% endtrans %}',
+                'There is 5 apples (Symfony)', ['count' => 5], ],
+            ['{% trans count into "fr"%}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples{% endtrans %}',
+                'There is no apples', ['count' => 0], ],
+            ['{% trans 5 into "fr"%}{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples{% endtrans %}',
+                'There is 5 apples', ],
             // trans filter
-            array('{{ "Hello"|trans }}', 'Hello'),
-            array('{{ name|trans }}', 'Symfony', array('name' => 'Symfony')),
-            array('{{ hello|trans({ \'%name%\': \'Symfony\' }) }}', 'Hello Symfony', array('hello' => 'Hello %name%')),
-            array('{% set vars = { \'%name%\': \'Symfony\' } %}{{ hello|trans(vars) }}', 'Hello Symfony', array('hello' => 'Hello %name%')),
-            array('{{ "Hello"|trans({}, "messages", "fr") }}', 'Hello'),
+            ['{{ "Hello"|trans }}', 'Hello'],
+            ['{{ name|trans }}', 'Symfony', ['name' => 'Symfony']],
+            ['{{ hello|trans({ \'%name%\': \'Symfony\' }) }}', 'Hello Symfony', ['hello' => 'Hello %name%']],
+            ['{% set vars = { \'%name%\': \'Symfony\' } %}{{ hello|trans(vars) }}', 'Hello Symfony', ['hello' => 'Hello %name%']],
+            ['{{ "Hello"|trans({}, "messages", "fr") }}', 'Hello'],
             // trans filter
-            array('{{ "{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples"|trans(count) }}', 'There is 5 apples', array('count' => 5)),
-            array('{{ text|trans(5, {\'%name%\': \'Symfony\'}) }}', 'There is 5 apples (Symfony)', array('text' => '{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples (%name%)')),
-            array('{{ "{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples"|trans(count, {}, "messages", "fr") }}', 'There is 5 apples', array('count' => 5)),
-        );
+            ['{{ "{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples"|trans(count) }}', 'There is 5 apples', ['count' => 5]],
+            ['{{ text|trans(5, {\'%name%\': \'Symfony\'}) }}', 'There is 5 apples (Symfony)', ['text' => '{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples (%name%)']],
+            ['{{ "{0} There is no apples|{1} There is one apple|]1,Inf] There is %count% apples"|trans(count, {}, "messages", "fr") }}', 'There is 5 apples', ['count' => 5]],
+        ];
     }
 
     private function getTemplate($template)
