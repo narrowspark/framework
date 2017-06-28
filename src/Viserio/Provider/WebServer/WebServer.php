d37d4cb680e77fc60bf3ed2d5af0b6cd846de5e3
Daniel Bannert <daniel.bannert@trnd.com>
2017-06-28 16:43:39 +0200

    more work

--- src/Viserio/Provider/WebServer/WebServer.php
+++ src/Viserio/Provider/WebServer/WebServer.php
@@ -4,4 +4,70 @@ namespace Viserio\Provider\WebServer;
 
 class WebServer
 {
+    /**
+     * @var int
+     */
+    private const STARTED = 0;
+
+    /**
+     * @var int
+     */
+    private const STOPPED = 1;
+
+    public function start()
+    {
+        # code...
+    }
+
+    public function stop()
+    {
+        # code...
+    }
+
+    public function run()
+    {
+        # code...
+    }
+
+    /**
+     * Check if a php build-in web server is running.
+     *
+     * @param string|null $pidFilePath
+     *
+     * @return bool
+     */
+    public function isRunning(string $pidFilePath = null): bool
+    {
+        $pidFile = $pidFile ?? $this->getDefaultPidFile();
+
+        if (! file_exists($pidFile)) {
+            return false;
+        }
+
+        $address  = file_get_contents($pidFile);
+        $pos      = strrpos($address, ':');
+        $hostname = substr($address, 0, $pos);
+        $port     = substr($address, $pos + 1);
+
+        if ($fp = @fsockopen($hostname, $port, $errno, $errstr, 1) !== false) {
+            fclose($fp);
+
+            return true;
+        }
+
+        unlink($pidFile);
+
+        return false;
+    }
+
+    /**
+     * Get the default pid file path.
+     *
+     * @return string
+     */
+    private function getDefaultPidFile(): string
+    {
+        return getcwd().'/.web-server-pid';
+    }
 }
+
