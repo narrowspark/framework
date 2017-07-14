d322e015f090aab69da5b6323a45e8c179542e34
Daniel Bannert <prisis33@gmail.com>
2017-07-14 14:37:02 +0000

    Apply fixes from StyleCI

--- src/Viserio/Component/Routing/ResourceRegistrar.php
+++ src/Viserio/Component/Routing/ResourceRegistrar.php
@@ -99,7 +99,7 @@ class ResourceRegistrar
         // are supported in the framework, but we need to know what name to use for a
         // place-holder on the route parameters, which should be the base resources.
         $baseResource  = explode('.', $name);
-        $base = $this->getResourceWildcard(end($baseResource));
+        $base          = $this->getResourceWildcard(end($baseResource));
 
         $defaults = $this->resourceDefaults;
 
