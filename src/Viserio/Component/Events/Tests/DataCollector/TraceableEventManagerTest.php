f2d0a1bbce7023faaf614bb1adefeece61730a44
Daniel Bannert <prisis33@gmail.com>
2017-07-24 22:20:37 +0000

    Apply fixes from StyleCI

--- src/Viserio/Component/Events/Tests/DataCollector/TraceableEventManagerTest.php
+++ src/Viserio/Component/Events/Tests/DataCollector/TraceableEventManagerTest.php
@@ -3,14 +3,7 @@ declare(strict_types=1);
 namespace Viserio\Component\Events\Tests\DataCollector;
 
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\EventDispatcher\Debug\TraceableEventDispatcher;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\EventDispatcher\Event;
-use Symfony\Component\Stopwatch\Stopwatch;
 
 class TraceableEventManagerTest extends TestCase
 {
-
 }
