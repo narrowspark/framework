6ea6ebbd2745121e70ae505fed66522b488793da
Daniel Bannert <daniel.bannert@trnd.com>
2017-07-31 18:13:44 +0200

    new exceptions

---
+++ src/Viserio/Component/Contracts/Log/Exception/Exception.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Component\Contracts\Log\Exception;
+
+/**
+ * Exception interface for all exceptions thrown by the component.
+ */
+interface Exception
+{
+}
---
+++ src/Viserio/Component/Contracts/Parsers/Exception/FileNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Component\Contracts\Parsers\Exception;
+
+use Exception;
+
+class FileNotFoundException extends Exception
+{
+}
