0cf3bf8991babff731bc6e3a7b11851a4ec0ac5e
Daniel Bannert <daniel.bannert@trnd.com>
2017-08-01 10:30:25 +0200

    adding more new exceptions

---
+++ src/Viserio/Component/Contracts/Pipeline/Exception/Exception.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Component\Contracts\Pipeline\Exception;
+
+/**
+ * Exception interface for all exceptions thrown by the component.
+ */
+interface Exception
+{
+}
