786c101605bdde84807178d6e0eb00bde8f4d136
Daniel Bannert <d.bannert@anolilab.de>
2017-09-20 22:51:19 +0200

    fixes #384

--- src/Viserio/Component/Queue/Tests/Fixture/InteractsWithQueue.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace Viserio\Component\Queue\Tests\Fixture;
-
-use Viserio\Component\Queue\AbstractInteractsWithQueue;
-
-class InteractsWithQueue extends AbstractInteractsWithQueue
-{
-}
