2296611d710280a04aba788a4975d125210d6721
Daniel Bannert <prisis33@gmail.com>
2017-09-24 22:20:37 +0000

    Apply fixes from StyleCI

--- src/Viserio/Component/Foundation/Bootstrap/LoadEnvironmentVariables.php
+++ src/Viserio/Component/Foundation/Bootstrap/LoadEnvironmentVariables.php
@@ -25,7 +25,6 @@ class LoadEnvironmentVariables implements BootstrapContract
         try {
             (new Dotenv($kernel->getEnvironmentPath(), $kernel->getEnvironmentFile()))->load();
         } catch (InvalidPathException $exception) {
-            //
         }
     }
 
