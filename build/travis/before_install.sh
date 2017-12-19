4b1314c83bbd0fc90943a579d6f2698f7f846502
Daniel Bannert <daniel.bannert@trnd.com>
2017-12-19 16:06:24 +0100

    update travis

--- build/travis/before_install.sh
+++ build/travis/before_install.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Disable xdebug when hhvm or when SEND_COVERAGE is false
-if [[ "$SEND_COVERAGE" = false ]]; then
-  phpenv config-rm xdebug.ini;
-fi
-
 echo "extension = memcached" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension = redis" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo date.timezone = Europe/Paris >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
