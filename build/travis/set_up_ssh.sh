9556200bac3538cb2f8eacf651a6774020c28546
Daniel Bannert <d.bannert@anolilab.de>
2018-03-21 22:34:37 +0100

    next try

--- build/travis/set_up_ssh.sh
+++ build/travis/set_up_ssh.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/XXXXX)"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -20,5 +21,18 @@ chmod 600 "$SSH_FILE" \
       "Host github.com" \
       "  IdentityFile $SSH_FILE" \
       "  LogLevel ERROR" >> ~/.ssh/config
+
+remove_sensitive_information() {
+    declare -r CENSOR_TEXT="[secure]";
+
+    while IFS="" read -r line; do
+        for text in "$@"; do
+            line="${line//${text}/$CENSOR_TEXT}"
+        done
+
+        printf "%s\n" "$line"
+    done
+
+}
 
 remove_sensitive_information "$GH_USER_EMAIL" "$GH_USER_NAME"
