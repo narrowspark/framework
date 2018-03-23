da5f58fdc917b09821bcf53d381d64277de5fed0
Daniel Bannert <daniel.bannert@trnd.com>
2018-03-23 09:27:59 +0100

    fix travis

--- build/travis/before_script.sh
+++
@@ -1,5 +0,0 @@
-if [[ "$INFECTION" = true ]]; then
-    wget https://github.com/infection/infection/releases/download/0.6.0/infection.phar
-    wget https://github.com/infection/infection/releases/download/0.6.0/infection.phar.pubkey
-    chmod +x infection.phar
-fi
