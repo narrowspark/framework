6f77c03af5f6eeeb3d92670de347a59af905213e
Daniel Bannert <d.bannert@anolilab.de>
2018-03-24 17:45:30 +0100

    next test

--- build/travis/components_phpunit.sh
+++ build/travis/components_phpunit.sh
@@ -4,20 +4,14 @@ source ./build/travis/utils.sh
 
 COMPONENTS=$(find src/Viserio -mindepth 3 -type f -name phpunit.xml.dist -printf '%h\n')
 
-for f in ./src/Viserio/*/*; do
-    if [[ -d "$f" && ! -L "$f" ]]; then
-        SLUG="$(basename "$f")";
-        TYPE="$(basename "${f%/*}")";
+try
+    php ./build/travis/build_packages.php HEAD^ $COMPONENTS
 
-        try
-            php ./build/travis/build-packages.php HEAD^ $COMPONENTS
-
-            composer validate "$f/composer.json" --strict
-            "$f" | composer install
-
-            tfold "$TESTSUITE" "$f | $TEST --verbose";
-        catch || {
-            exit 1
-        }
+    if [[ "$SETUP" = "high" ]]; then
+        echo "$COMPONENTS" | parallel --gnu -j10% "tfold {} 'cd {} && $COMPOSER_UP && $TEST --verbose'"
+    elif [[ "$SETUP" = "lowest" ]]; then
+        echo "$COMPONENTS" | parallel --gnu -j10% "tfold {} 'cd {} && $COMPOSER_UP --prefer-lowest --prefer-stable && $TEST --verbose'"
     fi
-done
+catch || {
+    exit 1
+}
