acf8e1edd5d0ee63b520eb20194938fd1e84d6d9
Daniel Bannert <d.bannert@anolilab.de>
2018-03-26 18:42:07 +0200

    change install of sodium

--- build/travis/configure_php.sh
+++ build/travis/configure_php.sh
@@ -7,18 +7,3 @@ fi
 echo "extension = memcached" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension = redis" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo date.timezone = Europe/Paris >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-
-# Install libsodium
-if [ ! -d "$HOME/.libsodium/lib" ]; then
-  git clone -b stable https://github.com/jedisct1/libsodium.git
-  cd libsodium
-  sudo ./configure --prefix="$HOME/.libsodium"
-  sudo make check
-  sudo make install
-
-  cd $HOME/.libsodium
-  pecl install -f libsodium
-else
-  cd $HOME/.libsodium
-  pecl install -f libsodium
-fi
