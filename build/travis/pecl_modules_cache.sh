9d29478e4e45ea6288b0b2a93caff93637cd353b
Daniel Bannert <daniel.bannert@trnd.com>
2018-03-26 17:24:55 +0200

    changes on cache pecl script

--- build/travis/pecl_modules_cache.sh
+++ build/travis/pecl_modules_cache.sh
@@ -3,7 +3,6 @@
 MODULE_CACHE_DIR=$HOME/.pecl_cache/`php-config --vernum`
 INI_DIR=$HOME/.pecl_cache/ini
 PHP_TARGET_DIR=`php-config --extension-dir`
-PHP_INI_FILE=`php -r "echo php_ini_loaded_file();"`
 
 if [ -d ${MODULE_CACHE_DIR} ]
 then
@@ -13,21 +12,18 @@ fi
 mkdir -p ${INI_DIR}
 mkdir -p ${MODULE_CACHE_DIR}
 
-for module in $MODULES
-do
+for module in $MODULES; do
   FILENAME=`echo $module|cut -d : -f 1`
-  PRIORITY=`echo $module|cut -d : -f 2`
-  PACKAGE=`echo $module|cut -d : -f 3`
-  if [ ! -f ${PHP_TARGET_DIR}/${FILENAME} ]
-  then
+  PACKAGE=`echo $module|cut -d : -f 2`
+
+  if [ ! -f ${PHP_TARGET_DIR}/${FILENAME} ]; then
     echo "$FILENAME not found in extension dir, compiling"
     printf "yes\n" | pecl install ${PACKAGE}
-    sed -i '1d' ${PHP_INI_FILE}
+  else
+    echo "Adding $FILENAME to php config"
+    echo "extension = $FILENAME" > ${INI_DIR}/${FILENAME}.ini
+    phpenv config-add ${INI_DIR}/${FILENAME}.ini
   fi
-
-  echo "Adding $FILENAME to php config"
-  echo "extension = $FILENAME" > ${INI_DIR}/${PRIORITY}-${FILENAME}.ini
-  phpenv config-add ${INI_DIR}/${PRIORITY}-${FILENAME}.ini
 
   cp ${PHP_TARGET_DIR}/${FILENAME} ${MODULE_CACHE_DIR}
 done
