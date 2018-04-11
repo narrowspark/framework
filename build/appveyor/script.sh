988c006c46c87a609a1a0e5026740e2aaeb390b0
Daniel Bannert <daniel.bannert@trnd.com>
2018-04-11 08:52:19 +0200

    use appveyor phpunit script

---
+++ build/appveyor/script.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+source ./build/appveyor/try_catch.sh
+
+for f in ./src/Viserio/*/*; do
+    if [[ -d "$f" && ! -L "$f" ]]; then
+        SLUG="$(basename "$f")";
+        TYPE="$(basename "${f%/*}")";
+
+        if [[ "$TYPE" = "Component" ]]; then
+            TESTSUITE="Narrowspark $SLUG Component Test Suite";
+        elif [[ "$TYPE" = "Bridge" ]]; then
+            TESTSUITE="Narrowspark $SLUG Bridge Test Suite";
+        elif [[ "$TYPE" = "Provider" ]]; then
+            TESTSUITE="Narrowspark $SLUG Provider Test Suite";
+        fi
+
+        try
+            %PHP% -c ./phpunit.xml.dist --testsuite="$TESTSUITE";
+        catch || {
+            exit 1
+        }
+    fi
+done
