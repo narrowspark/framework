1366b2337c91b2bb99f59c889d790cfb80a15fac
Daniel Bannert <prisis@users.noreply.github.com>
2018-04-11 11:01:21 +0200

    Add phpunit run for components for appveyor(#686)

---
+++ build/appveyor/script.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+source ./build/appveyor/try_catch.sh
+
+for f in ./src/Viserio/*/*; do
+    if [[ -d "$f" && ! -L "$f" ]]; then
+        SLUG="$(basename "$f")";
+        TYPE="$(basename "${f%/*}")";
+
+        if [[ "$TYPE" = "Component" ]]; then
+            TESTSUITE="Narrowspark $SLUG Component Test Suite";
+        elif [[ "$TYPE" = "Bridge" ]]; then
+            TESTSUITE="Narrowspark $SLUG Bridge Test Suite";
+        elif [[ "$TYPE" = "Provider" ]]; then
+            TESTSUITE="Narrowspark $SLUG Provider Test Suite";
+        fi
+
+        echo "";
+        echo -e "$TESTSUITE";
+        echo "";
+
+        try
+            sh vendor/bin/phpunit --verbose -c ./phpunit.xml.dist --testsuite="$TESTSUITE";
+        catch || {
+            exit 1
+        }
+    fi
+done
