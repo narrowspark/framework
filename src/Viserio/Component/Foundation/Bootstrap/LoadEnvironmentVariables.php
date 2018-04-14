3c0e6c46af7222c2546eb61bdc46c884f050089f
Daniel Bannert <d.bannert@anolilab.de>
2018-04-14 16:49:43 +0200

    cs fix

--- src/Viserio/Component/Foundation/Bootstrap/LoadEnvironmentVariables.php
+++ src/Viserio/Component/Foundation/Bootstrap/LoadEnvironmentVariables.php
@@ -27,7 +27,6 @@ class LoadEnvironmentVariables implements BootstrapContract
         try {
             (new Dotenv($kernel->getEnvironmentPath(), $kernel->getEnvironmentFile()))->load();
         } catch (InvalidPathException $exception) {
-            //
         } catch (InvalidFileException $exception) {
             Dumper::dump($exception->getMessage());
             die();
