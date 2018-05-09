d59a2387cf7a236ad0662802771505d271b1ec1f
Daniel Bannert <daniel.bannert@trnd.com>
2018-05-09 09:49:56 +0200

    fix phpdoc

--- src/Viserio/Component/Http/Response.php
+++ src/Viserio/Component/Http/Response.php
@@ -26,7 +26,8 @@ class Response extends AbstractMessage implements ResponseInterface, StatusCodeI
      * @param null|\Psr\Http\Message\StreamInterface|resource|string $body    Stream identifier and/or actual stream resource
      * @param string                                                 $version protocol version
      *
-     * @throws \Narrowspark\HttpStatus\Exception\InvalidArgumentException on any invalid element
+     * @throws \Narrowspark\HttpStatus\Exception\InvalidArgumentException          on any invalid status
+     * @throws \Viserio\Component\Contract\Http\Exception\InvalidArgumentException on any invalid content
      */
     public function __construct(
         int $status = self::STATUS_OK,
