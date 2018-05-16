b339754e65bfbe44853b979715d4608e7ef68576
Daniel Bannert <daniel.bannert@trnd.com>
2018-05-16 10:20:48 +0200

    fixes #546

---
+++ src/Viserio/Component/Contract/Translation/Extractor.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Component\Contract\Translation;
+
+interface Extractor
+{
+    /**
+     * Extracts translation messages from files, a file or a directory to a array.
+     *
+     * @param string|array $resource Files, a file or a directory
+     *
+     * @return array
+     */
+    public function extract($resource): array;
+
+    /**
+     * Sets the prefix that should be used for new found messages.
+     *
+     * @param string $prefix
+     */
+    public function setPrefix(string $prefix): void;
+}
---
+++ src/Viserio/Component/Translation/Extractor/AbstractFileExtractor.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Component\Translation;
+
+use Viserio\Component\Contract\Translation\Extractor as ExtractorContract;
+
+abstract class AbstractFileExtractor implements ExtractorContract
+{
+
+}
