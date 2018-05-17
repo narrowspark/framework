f3c7a391aca52cdfcd16b19216b9cca7e1257109
Daniel Bannert <daniel.bannert@trnd.com>
2018-05-17 15:14:03 +0200

    fixes #708

---
+++ src/Viserio/Component/Translation/Parser/YamlParser.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Component\Translation\Parser;
+
+use Symfony\Component\Yaml\Yaml;
+use Viserio\Component\Parser\Parser\YamlParser as BaseYamlParser;
+
+class YamlParser extends BaseYamlParser
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $flags = Yaml::PARSE_CONSTANT;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setFlags(int $flags): void
+    {
+        if ($flags === Yaml::PARSE_CONSTANT) {
+            return;
+        }
+
+        parent::setFlags($flags);
+    }
+}
