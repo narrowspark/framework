e44b60d82e70f8fbd28e8bc5d0be6d22d1b7cda7
danielbannert <daniel.bannert@limango.de>
2018-07-23 16:29:34 +0200

    removed

--- src/Viserio/Bridge/Twig/Tests/Provider/ConsoleCommandsServiceProviderTest.php
+++ src/Viserio/Bridge/Twig/Tests/Provider/ConsoleCommandsServiceProviderTest.php
@@ -8,7 +8,6 @@ use Twig\Loader\ArrayLoader;
 use Viserio\Bridge\Twig\Command\DebugCommand;
 use Viserio\Bridge\Twig\Command\LintCommand;
 use Viserio\Bridge\Twig\Provider\ConsoleCommandsServiceProvider;
-use Viserio\Bridge\Twig\Provider\TwigBridgeServiceProvider;
 use Viserio\Component\Console\Application;
 use Viserio\Component\Console\Provider\ConsoleServiceProvider;
 use Viserio\Component\Container\Container;
@@ -21,7 +20,6 @@ final class ConsoleCommandsServiceProviderTest extends TestCase
     public function testGetServices(): void
     {
         $container = new Container();
-        $container->register(new TwigBridgeServiceProvider());
         $container->register(new ConsoleServiceProvider());
         $container->register(new ConsoleCommandsServiceProvider());
         $container->instance(Environment::class, new Environment(new ArrayLoader([])));
