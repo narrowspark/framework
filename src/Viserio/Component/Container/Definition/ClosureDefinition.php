d1ecc440491b42f9f4f0f72f79c71dadb53b3c71
danielbannert <daniel.bannert@limango.de>
2018-10-02 17:17:36 +0200

    working

---
+++ src/Viserio/Component/Container/Definition/ClosureDefinition.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Component\Container\Definition;
+
+use Viserio\Component\Contract\Container\Compiler\Definition as DefinitionContract;
+
+/**
+ * @internal
+ */
+final class ClosureDefinition extends ReflectionResolver implements DefinitionContract
+{
+
+}
---
+++ src/Viserio/Component/Container/Definition/FunctionDefinition.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+namespace Viserio\Component\Container\Definition;
+
+use Psr\Container\ContainerInterface;
+use Viserio\Component\Container\Definition\Traits\DefinitionTrait;
+use Viserio\Component\Container\Definition\Traits\DeprecationTrait;
+use Viserio\Component\Container\Definition\Traits\ResolveParameterClassTrait;
+use Viserio\Component\Container\Reflection\ReflectionFactory;
+use Viserio\Component\Container\Reflection\ReflectionResolver;
+use Viserio\Component\Contract\Container\Compiler\Definition as DefinitionContract;
+
+/**
+ * @internal
+ */
+final class FunctionDefinition extends ReflectionResolver implements DefinitionContract
+{
+    use DefinitionTrait;
+    use DeprecationTrait;
+    use ResolveParameterClassTrait;
+
+    /**
+     * Default deprecation template.
+     *
+     * @var string
+     */
+    private $defaultDeprecationTemplate = 'The [%s] binding is deprecated. You should stop using it, as it will soon be removed.';
+
+    /**
+     * Create a new Factory Definition instance.
+     *
+     * @param string                $name
+     * @param array|callable|string $value
+     * @param int                   $type
+     */
+    public function __construct(string $name, $value, int $type)
+    {
+        $this->name = $name;
+        $this->type = $type;
+
+        $this->reflector = ReflectionFactory::getFunctionReflector($value);
+        $this->parameters = ReflectionFactory::getParameters($this->reflector);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolve(ContainerInterface $container, array $parameters = []): void
+    {
+        $this->value = $this->resolveReflectionFunction($this->reflector, $this->parameters, $parameters);
+
+        $this->extend($this->value, $container);
+
+        $this->resolved = true;
+    }
+}
