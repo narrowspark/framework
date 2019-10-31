a332f7db92a08496a947d1c92788188742e75c1e
prisis <d.bannert@anolilab.de>
2019-10-31 08:40:28 +0100

    build : update appveyor and travis

--- build/travis/phpunit.sh
+++ build/travis/phpunit.sh
@@ -3,6 +3,8 @@
 source ./build/travis/try_catch.sh
 source ./build/travis/tfold.sh
 
+tfold 'composer update' $COMPOSER_UP
+
 if [[ "$SETUP" = "high" ]]; then
     [[ $LEGACY ]] && EXCLUDE_GROUP=" --exclude-group $LEGACY" || EXCLUDE_GROUP="";
 
@@ -10,12 +12,14 @@ if [[ "$SETUP" = "high" ]]; then
 
     COMPONENTS=$(git diff --name-only src/ | grep composer.json || true)
 
-    if [[ $COMPONENTS && $LEGACY && $TRAVIS_PULL_REQUEST != false ]]; then
+    if [[ $COMPONENTS && $LEGACY && $TRAVIS_BRANCH != master && $TRAVIS_PULL_REQUEST != false ]]; then
         export FLIP='🙃'
 
         NARROWSPARK_VERSION=$(echo $NARROWSPARK_VERSION | awk '{print $1 - 1}')
 
         echo -e "\\n\\e[33;1mChecking out Viserio $NARROWSPARK_VERSION and running tests with patched components as deps\\e[0m"
+
+        export COMPOSER_ROOT_VERSION=$NARROWSPARK_VERSION.x-dev
 
         git fetch --depth=2 origin $NARROWSPARK_VERSION
         git checkout -m FETCH_HEAD
@@ -27,5 +31,10 @@ if [[ "$SETUP" = "high" ]]; then
 
     [[ ! $X ]] || (exit 1)
 else
+    [[ -e ~/php-ext/composer-lowest.lock.tar ]] && tar -xf ~/php-ext/composer-lowest.lock.tar
+    tar -cf ~/php-ext/composer-lowest.lock.tar --files-from /dev/null
+    php ./build/travis/rm-invalid-lowest-lock-files.php $COMPONENTS
+
     echo "$COMPONENTS" | parallel --gnu "tfold {} 'cd {} && ([ -e composer.lock ] && composer validate --strict && ${COMPOSER_UP/update/install} || $COMPOSER_UP --prefer-lowest --prefer-stable) && $PHPUNIT'"
+    echo "$COMPONENTS" | xargs -n1 -I{} tar --append -f ~/php-ext/composer-lowest.lock.tar {}/composer.lock
 fi
